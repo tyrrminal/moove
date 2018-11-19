2c22dd9f49634f06167e340728d680df8e5f81f6
Mark Tyrrell <MTyrrell@concertpharma.com>
2018-11-19 07:18:24 -0500

    providing simple accessor for groups without counts

---
+++ lib/CardioTracker/Model/ResultSet/EventResultGroup.pm
@@ -0,0 +1,12 @@
+package CardioTracker::Model::ResultSet::EventResultGroup;
+
+use base qw(DBIx::Class::ResultSet);
+
+use DCS::Constants qw(:boolean :existence);
+
+sub missing_count {
+  my $self=shift;
+  return $self->search({count => {'=', $NULL}});
+}
+
+1;
