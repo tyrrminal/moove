8957deb577ab0907188a3f47de3d43013e7e7015
Mark Tyrrell <MTyrrell@concertpharma.com>
2018-11-19 07:20:17 -0500

    providing simple accessor for events by name & year providing simple accessor for events missing gender groups

---
+++ lib/CardioTracker/Model/ResultSet/Event.pm
@@ -0,0 +1,34 @@
+package CardioTracker::Model::ResultSet::Event;
+
+use base qw(DBIx::Class::ResultSet);
+
+use DCS::Constants qw(:boolean :existence);
+
+sub find_event {
+  my $self=shift;
+  my ($year,$name) = @_;
+
+  my ($event) = grep { $_->scheduled_start->year == $year } $self->search({name => $name})->all;
+  return $event;
+}
+
+sub is_missing_gender_group {
+  my $self=shift;
+
+  $self->search(\["
+    me.id IN (
+      SELECT e.id 
+      FROM `event` e
+      JOIN gender g
+      WHERE NOT EXISTS (
+        SELECT *
+          FROM event_result_group erg
+          WHERE erg.event_id=e.id
+          AND erg.gender_id=g.id
+          AND erg.division_id IS NULL
+      )
+    )
+  "]);
+}
+
+1;
