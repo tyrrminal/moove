cf00dffc3d20bf80d5e0032a9abacda0e2a53557
Mark Tyrrell <objcow@gmail.com>
2018-11-20 16:36:50 -0500

    capturing more activity data in schema

--- lib/CardioTracker/Model/Result/Activity.pm
+++ lib/CardioTracker/Model/Result/Activity.pm
@@ -79,6 +79,11 @@ __PACKAGE__->table("activity");
   is_foreign_key: 1
   is_nullable: 1
 
+=head2 note
+
+  data_type: 'mediumtext'
+  is_nullable: 1
+
 =cut
 
 __PACKAGE__->add_columns(
@@ -98,6 +103,8 @@ __PACKAGE__->add_columns(
   { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
   "event_id",
   { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "note",
+  { data_type => "mediumtext", is_nullable => 1 },
 );
 
 =head1 PRIMARY KEY
@@ -210,8 +217,8 @@ Composing rels: L</user_activities> -> user
 __PACKAGE__->many_to_many("users", "user_activities", "user");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2018-11-19 17:38:07
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:TyoHMYOs+uLYn2Uvx8sWkg
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2018-11-20 21:32:42
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:jdwo4ap6NV1nyD2LS1luwg
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
--- lib/CardioTracker/Model/Result/Result.pm
+++ lib/CardioTracker/Model/Result/Result.pm
@@ -64,6 +64,11 @@ __PACKAGE__->table("result");
   data_type: 'time'
   is_nullable: 1
 
+=head2 heart_rate
+
+  data_type: 'integer'
+  is_nullable: 1
+
 =cut
 
 __PACKAGE__->add_columns(
@@ -75,6 +80,8 @@ __PACKAGE__->add_columns(
   { data_type => "time", is_nullable => 0 },
   "pace",
   { data_type => "time", is_nullable => 1 },
+  "heart_rate",
+  { data_type => "integer", is_nullable => 1 },
 );
 
 =head1 PRIMARY KEY
@@ -137,8 +144,8 @@ __PACKAGE__->has_many(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2018-11-19 17:03:55
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:qypdoSDGVBY6bVrUl5QUPA
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2018-11-20 21:32:42
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:uetz95tb9gHqCiXVrPfrsA
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
