fecdc6157c64aff209c64c8cbf7415a80607c069
Mark Tyrrell <objcow@gmail.com>
2019-02-19 10:03:39 -0500

    improve output formatting

--- lib/CardioTracker/Command/status.pm
+++ lib/CardioTracker/Command/status.pm
@@ -51,27 +51,19 @@ sub run {
   my $week_activities = $activities->search({ start_time => {'>' => $now->clone->truncate(to => 'local_week')->strftime('%F') } });
   my $day_activities  = $activities->search({ start_time => {'>' => $now->strftime('%F') } });
 
-  my $yd = sum(map { $_->distance->normalized_value } $year_activities->all) - $doy;
-  my $md = sum(map { $_->distance->normalized_value } $month_activities->all) - $dom;
-  my $wd = sum(map { $_->distance->normalized_value } $week_activities->all) - $dow;
-  my $dd = sum(map { $_->distance->normalized_value } $day_activities->all) - 1;
+  my $yd = sum(0,map { $_->distance->normalized_value } $year_activities->all ) - $doy;
+  my $md = sum(0,map { $_->distance->normalized_value } $month_activities->all) - $dom;
+  my $wd = sum(0,map { $_->distance->normalized_value } $week_activities->all ) - $dow;
+  my $dd = sum(0,map { $_->distance->normalized_value } $day_activities->all  ) - 1;
 
   say $activity_type;
-  print "   [Year]";
-  print color('bold red') if($yd<0);
-  say sprintf("%7s", sprintf("%+.2f", $yd) );
-  print color('reset');
-  print "  [Month]";
-  print color('bold red') if($md<0);
-  say sprintf("%7s", sprintf("%+.2f", $md) );
-  print color('reset');
-  print "   [Week]";
-  print color('bold red') if($wd<0);
-  say sprintf("%7s", sprintf("%+.2f", $wd));
-  print color('reset');
-  print "    [Day]";
-  print color('bold red') if($dd<0);
-  say sprintf("%7s", sprintf("%+.2f", $dd));
+  foreach (['Year',$yd],['Month',$md],['Week',$wd],['Day',$dd]) {
+    my ($label,$v) = @$_;
+    print sprintf("  %7s", "[$label]");
+    print color('bold red') if($v<0);
+    say sprintf("%7s", sprintf("%+.2f", $v) );
+    print color('reset');
+  }
 }
 
 1;
