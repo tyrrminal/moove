9c7a9b4542b0dcf8b5719dd7506e8b81232c59be
Mark <mark@digicow.net>
2019-07-09 08:52:11 -0400

    conforming to updated API for get_activities

--- lib/CardioTracker/Controller/API/V1/Legacy.pm
+++ lib/CardioTracker/Controller/API/V1/Legacy.pm
@@ -42,41 +42,39 @@ sub get_activities {
   my $c = shift->openapi->valid_input or return;
 
   my $u = $c->app->model('User')->find({username => $c->validation->param('username')});
-  return $c->render_error(404, "User not found") unless($u);
+  return $c->render_error(404, "User not found") unless ($u);
 
-  my ($page,$per_page) = ($c->validation->param('page') || 0, $c->validation->param('perPage') || 10);
-  my $offset = max($page-1,0)*$per_page;
+  my ($page, $per_page) = ($c->validation->param('page') || 1, $c->validation->param('perPage') || 10);
+  my $offset = max($page - 1, 0) * $per_page;
 
   my @activities;
-  my %max = map { $_->description => 0 } $c->app->model('ActivityType')->all;
-  my $i=0;
-  foreach my $activity ($c->app->model('Activity')->whole->for_user($u)->core->completed->ordered->slice(0,$offset+$per_page-1)->all) {
-    my $t = $activity->activity_type->description;
+  my %max = map {$_->description => undef} $c->app->model('ActivityType')->all;
+  my $i = 0;
+  foreach
+    my $activity ($c->app->model('Activity')->whole->for_user($u)->core->completed->ordered->slice(0, $offset + $per_page - 1)->all)
+  {
+    my $t         = $activity->activity_type->description;
     my $prior_max = $max{$t};
-    $max{$t} = max($max{$t}, $activity->distance->normalized_value);
-    next unless(++$i>$offset);
-    my @keys = $activity->is_running_activity ? qw(running run_max) : qw(cycling cycling_max);
-    my $a = {
-      date => $activity->start_time->strftime('%D'),
-      type => $t,
-      $keys[0] => $activity->distance->normalized_value,
-      $keys[1] => $prior_max,
-      event_id => $activity->has_event_visible_to($c->app->current_user) ? $activity->event->id : undef,
-      event_name => $activity->has_event_visible_to($c->app->current_user) ? $activity->event->name : undef
-    };
+    if ($activity->distance->normalized_value > (defined($prior_max) ? $prior_max->distance->normalized_value : 0)) {
+      $max{$t} = $activity;
+    }
+    next unless (++$i > $offset);
     
+    my $a = $activity->to_hash;
+    $a->{prior_distance_pr} = $prior_max->to_hash if (defined($prior_max));
     push(@activities, $a);
   }
 
-  return $c->render(openapi => {
+  return $c->render(
+    openapi => {
     activities => \@activities,
-    _meta => {
-      page => $page,
+      _meta      => {
+        page    => $page,
       perPage => $per_page,
-      count => scalar @activities,
-      distance_units => $c->app->model('UnitOfMeasure')->normalization_unit->abbreviation
+        count   => scalar @activities
+      }
     }
-  })
+    );
 }
 
 1;
