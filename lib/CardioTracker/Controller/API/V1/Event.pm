d3b976ba12dc696a4a414d15770b6e04d0635e5f
Mark <mark@digicow.net>
2019-07-09 08:34:42 -0400

    adding Event API implementation

---
+++ lib/CardioTracker/Controller/API/V1/Event.pm
@@ -0,0 +1,82 @@
+package CardioTracker::Controller::API::V1::Event;
+use Mojo::Base 'Mojolicious::Controller';
+
+use DCS::Constants qw(:boolean);
+
+sub get {
+  my $self = shift;
+  my $c = $self->openapi->valid_input or return;
+  my $message;
+
+  my $event_id = $c->validation->param('id');
+  my $user_id  = $c->validation->param('user');
+
+  if (my $u = $c->model('User')->find($user_id)) {
+    if (my ($er) = $c->model('EventRegistration')->search({event_id => $event_id})->for_user($u)->visible_to($c->current_user)) {
+      my $h = {
+        registration => $er->to_hash,
+        event => $er->event->to_hash
+      };
+      if(my $activity = $er->event->activities->search({ user_id => $u->id },{ join => 'user_activities' })->first) {
+        $h->{activity} = $activity->to_hash(event => $FALSE) ;
+
+        if(my @results = $activity->result->event_results) {
+          $h->{results} = [map { $_->to_hash } @results]
+        }
+      }
+
+      return $c->render(openapi => $h);
+    } else {
+      $message = "Event not found";
+    }
+  } else {
+    $message = "User not found";
+  }
+
+  return $c->render(
+    status  => 404,
+    openapi => {
+      message => $message
+    }
+  );
+}
+
+sub list {
+  my $self = shift;
+  my $c = $self->openapi->valid_input or return;
+  my $message;
+
+  my $user_id  = $c->validation->param('user');
+
+  my $i = 0;
+  if (my $u = $c->model('User')->find($user_id)) {
+    my @events;
+    foreach my $er ($c->model('EventRegistration')->for_user($u)->visible_to($c->current_user)->ordered('-desc')) {
+      my $h = {
+        registration => $er->to_hash(donations => $FALSE),
+        event => $er->event->to_hash
+      };
+      if(my $activity = $er->event->activities->search({ user_id => $u->id },{ join => 'user_activities' })->first) {
+        $h->{activity} = $activity->to_hash(event => $FALSE) ;
+
+        if(my @results = $activity->result->event_results) {
+          $h->{results} = [map { $_->to_hash } @results]
+        }
+      }
+
+      push(@events, $h);
+    }
+    return $c->render(openapi => [@events]);
+  } else {
+    $message = "User not found";
+  }
+
+  return $c->render(
+    status  => 404,
+    openapi => {
+      message => $message
+    }
+  );
+}
+
+1;
