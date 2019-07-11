144c5644ad5405f99a0fcd336fbafc0c599ebeb4
Mark <mark@digicow.net>
2019-07-11 07:19:47 -0400

    adding new Login/Registration pages

---
+++ cardiotracker-ui/src/components/auth/Register.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h4>Register</h4>
+    <form @submit.prevent="register">
+      <label for="name">Name</label>
+      <div>
+        <input id="name" type="text" v-model="name" required autofocus />
+      </div>
+      <label for="name">Username</label>
+      <div>
+        <input id="username" type="text" v-model="username" required autofocus />
+      </div>
+      <label for="password">Password</label>
+      <div>
+        <input id="password" type="password" v-model="password" required />
+      </div>
+      <label for="password">Confirm Password</label>
+      <div>
+        <input id="password-confirm" type="password" v-model="password_confirmation" required />
+      </div>
+      <div>
+        <button type="submit">Register</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      username: "",
+      password: "",
+      password_confirmation: ""
+    };
+  },
+  methods: {
+    register: function() {
+      let data = {
+        name: this.name,
+        username: this.username,
+        password: this.password
+      };
+      this.$store
+        .dispatch("register", data)
+        .then(() => this.$router.push("/"))
+        .catch(err => console.log(err));
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
