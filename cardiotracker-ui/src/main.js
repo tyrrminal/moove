18fdadf19d0529035cd9d67b887711472cde75ef
Mark <mark@digicow.net>
2019-07-14 11:06:31 -0400

    adding font-awesome to app

--- cardiotracker-ui/src/main.js
+++ cardiotracker-ui/src/main.js
@@ -19,6 +19,10 @@ Vue.use(require('vue-moment'));
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false;
 
 new Vue({
