a775187c902501e3daaa873f0b2f3ff830ddff62
Mark <mark@digicow.net>
2019-07-14 11:16:45 -0400

    adding support for eventreg series/sequence navigation

--- lib/CardioTracker/Model/ResultSet/EventRegistration.pm
+++ lib/CardioTracker/Model/ResultSet/EventRegistration.pm
@@ -2,20 +2,77 @@ package CardioTracker::Model::ResultSet::EventRegistration;
 
 use base qw(DBIx::Class::ResultSet);
 
+use DateTime::Format::MySQL;
+
+sub before {
+  my $self = shift;
+  my ($event) = @_;
+
+  return $self->search(
+    {
+      'event.scheduled_start' => {'<' => DateTime::Format::MySQL->format_datetime($event->scheduled_start)}
+    }, {
+      join => 'event'
+    }
+  )->ordered('-desc');
+}
+
+sub after {
+  my $self = shift;
+  my ($event) = @_;
+
+  return $self->search(
+    {
+      'event.scheduled_start' => {'>' => DateTime::Format::MySQL->format_datetime($event->scheduled_start)}
+    }, {
+      join => 'event'
+    }
+  )->ordered('-asc');
+}
+
+sub in_sequence {
+  my $self = shift;
+  my ($sequence_id) = @_;
+
+  return $self->search(
+    {
+      '-and' => [
+        {'event_group.event_sequence_id' => $sequence_id},
+        {'event_group.event_sequence_id' => {'<>' => undef }}
+      ]
+    }, {
+      join => {event => 'event_group'}
+    }
+  );
+}
+
+sub in_series {
+  my $self = shift;
+  my ($series_id) = @_;
+
+  return $self->search(
+    {
+      'event_group_series.event_series_id' => $series_id
+    }, {
+      join => {event => {event_group => 'event_group_series'}}
+    }
+  );
+}
+
 sub for_user {
   my $self = shift;
-  my ($user) = shift;
+  my ($user) = @_;
 
   return $self->search(
     {
       user_id => $user->id
     }
-    );
+  );
 }
 
 sub visible_to {
   my $self = shift;
-  my ($user) = shift;
+  my ($user) = @_;
 
   return $self->search(
     {
@@ -24,20 +81,20 @@ sub visible_to {
         user_id   => $user->id
       ]
     }
-    );
+  );
 }
 
 sub ordered {
   my $self = shift;
-  my $direction = shift // '-asc';
+  my ($direction) = (@_, '-asc');
 
   return $self->search(
     {},
     {
       join     => 'event',
-      order_by => { $direction => 'event.scheduled_start'}
+      order_by => {$direction => 'event.scheduled_start'}
     }
-    );
+  );
 }
 
 1;
