c375758824207b48012d55d20750a64585e2cc80
Mark <mark@digicow.net>
2019-07-14 11:30:05 -0400

    breaking down Event page into reuseable cards and fragmentz

---
+++ cardiotracker-ui/src/components/activity/cards/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-card title="Activity" class="activity">
+    <b-list-group flush>
+      <b-list-group-item><label>Actual distance</label>: {{ activity.distance | format_distance }}</b-list-group-item>
+      <b-list-group-item><label>Moving Time</label>: {{ activity.result.net_time }}</b-list-group-item>
+      <b-list-group-item v-if="activity.result.gross_time"><label>Total Time</label>: {{ activity.result.gross_time }}</b-list-group-item>
+      <b-list-group-item v-if="activity.activity_type.description === 'Run'"><label>Pace</label>: {{ activity.result.pace }}</b-list-group-item>
+      <b-list-group-item v-else><label>Speed</label>: {{ activity.result.speed | format_distance }}</b-list-group-item>
+      <b-list-group-item v-if="activity.temperature"><label>Temperature</label>: {{ activity.temperature }}&deg;F</b-list-group-item>
+    </b-list-group>
+  </b-card>
+</template>
+
+<script>
+import '@/filters/event_filters.js';
+
+export default {
+  props: {
+    activity: Object
+  }
+}
+</script>
+
+<style scoped>
+.activity .list-group-item {
+  text-align: left;
+}
+.activity label {
+  width: 8rem;
+  text-align: right;
+}
+</style>
---
+++ cardiotracker-ui/src/components/event/cards/Fundraising.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-card title="Donations" class="fundraising">
+    <b-list-group flush>
+      <b-list-group-item><label>Required</label>: ${{ fundraising.minimum }}</b-list-group-item>
+      <b-list-group-item><label>Raised</label>: ${{ fundraising.total }}</b-list-group-item>
+      <b-list-group-item>{{ fundraising.donations.map(d => d.person.first_name + "&nbsp;" + d.person.last_name).join(", ") }}</b-list-group-item>
+    </b-list-group>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: {
+    fundraising: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
