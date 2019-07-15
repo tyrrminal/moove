63d64d5f6d26c565ed45ddfad299e7f4513add92
Mark <mark@digicow.net>
2019-07-15 13:39:48 -0400

    omit address display when address not supplied

--- cardiotracker-ui/src/components/event/cards/EventDetails.vue
+++ cardiotracker-ui/src/components/event/cards/EventDetails.vue
@@ -1,6 +1,5 @@
 <template>
-  <b-card :title="event.name" :sub-title="this.$options.filters.format_distance(event.distance) + ' ' + event.event_type.description" class="mb-2" bg-variant="dark" text-variant="white">
-    <h6 slot="footer">{{ event.scheduled_start | moment("M/D/YY h:mma") }} &mdash; {{ event.address.city }}, {{ event.address.state }}</h6>
+    <h6 slot="footer">{{ event.scheduled_start | moment("M/D/YY h:mma") }}<template v-if="event.hasOwnProperty('address')"> &mdash; {{ event.address.city }}, {{ event.address.state }}</template></h6>
   </b-card>
 </template>
 
