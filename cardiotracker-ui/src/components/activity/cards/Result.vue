276defedff0315eca7e8cbb9af2d9bf9c29a0151
Mark <mark@digicow.net>
2019-07-15 12:58:13 -0400

    display times more concisely

--- cardiotracker-ui/src/components/activity/cards/Result.vue
+++ cardiotracker-ui/src/components/activity/cards/Result.vue
@@ -2,8 +2,8 @@
   <b-card title="Activity" class="activity">
     <b-list-group flush>
       <b-list-group-item><label>Actual distance</label>: {{ activity.distance | format_distance }}</b-list-group-item>
-      <b-list-group-item><label>Moving Time</label>: {{ activity.result.net_time }}</b-list-group-item>
-      <b-list-group-item v-if="activity.result.gross_time"><label>Total Time</label>: {{ activity.result.gross_time }}</b-list-group-item>
+      <b-list-group-item><label><template v-if="hasStoppedTime">Moving </template>Time</label>: {{ activity.result.net_time }}</b-list-group-item>
+      <b-list-group-item v-if="activity.result.gross_time && hasStoppedTime"><label>Total Time</label>: {{ activity.result.gross_time }}</b-list-group-item>
       <b-list-group-item v-if="activity.activity_type.description === 'Run'"><label>Pace</label>: {{ activity.result.pace }}</b-list-group-item>
       <b-list-group-item v-else><label>Speed</label>: {{ activity.result.speed | format_distance }}</b-list-group-item>
       <b-list-group-item v-if="activity.temperature"><label>Temperature</label>: {{ activity.temperature }}&deg;F</b-list-group-item>
@@ -17,6 +17,11 @@ import '@/filters/event_filters.js';
 export default {
   props: {
     activity: Object
+  },
+  computed: {
+    hasStoppedTime: function() {
+      return this.activity.result.gross_time != this.activity.result.net_time
+    }
   }
 }
 </script>
