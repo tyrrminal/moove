6a8434be79beb92bafac288f6e727bfdddb3f32c
Mark <mark@digicow.net>
2019-07-16 08:47:00 -0400

    removing vue-cookie reformatting code

--- cardiotracker-ui/src/main.js
+++ cardiotracker-ui/src/main.js
@@ -3,38 +3,38 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// Moment (Date formatting)
+Vue.use(require('vue-moment'));
+
+// Headful (page titles & meta)
 import vueHeadful from 'vue-headful';
 Vue.component('vue-headful', vueHeadful);
 
-var VueCookie = require('vue-cookie');
-Vue.use(VueCookie);
-
+// Axios (Networking)
 import VueAxios from "vue-axios";
 import axios from './services/axios';
-
 Vue.use(VueAxios, axios);
 
+// Layout and design
 import BootstrapVue from "bootstrap-vue";
-
 Vue.use(BootstrapVue);
-Vue.use(require('vue-moment'));
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+// Symbols and icons
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
 import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faAngleRight, faAngleLeft, faAngleDoubleRight, faAngleDoubleLeft, faExternalLinkAlt )
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
+// Vue
 Vue.config.productionTip = false;
-
 new Vue({
   router,
   store,
