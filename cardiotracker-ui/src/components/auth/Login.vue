3952362e5adf25d65aa110200579f1b20721fe44
Mark <mark@digicow.net>
2019-07-16 12:26:54 -0400

    adding page title on login page

--- cardiotracker-ui/src/components/auth/Login.vue
+++ cardiotracker-ui/src/components/auth/Login.vue
@@ -1,5 +1,6 @@
 <template>
   <b-container>
+    <vue-headful title="Cardio Tracker Login" />
     <b-row>
       <b-col sm="9" md="7" lg="5" class="mx-auto">
         <b-card title="Login" class="card-signin my-5">
