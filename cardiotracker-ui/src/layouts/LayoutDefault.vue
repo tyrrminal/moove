aecbbdcfb2b163b993e5c93b25f885f1ba5c03ad
Mark <mark@digicow.net>
2019-07-17 13:58:10 -0400

    adding default layout and component-configurable sidebar

---
+++ cardiotracker-ui/src/layouts/LayoutDefault.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container fluid>
+    <b-row >
+      <b-col sm="2">
+        <slot name="sidebar" />
+      </b-col>
+      <b-col sm="10" class="mt-2">
+        <slot />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
