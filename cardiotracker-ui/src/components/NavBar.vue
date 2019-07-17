5bd4c37c382d0b78ac1a8b99f0f71ad77463c170
Mark <mark@digicow.net>
2019-07-17 13:50:45 -0400

    removing nonfunctional sticky attribute removing bottom margin

--- cardiotracker-ui/src/components/NavBar.vue
+++ cardiotracker-ui/src/components/NavBar.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="secondary" class="mb-2" sticky>
+    <b-navbar toggleable="lg" type="dark" variant="secondary">
       <b-navbar-brand :to="{ name: 'home' }">Moo've</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-
+        
         <b-navbar-nav v-if="isLoggedIn">
           <b-nav-item>Activities</b-nav-item>
           <b-nav-item>Events</b-nav-item>
