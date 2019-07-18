9190a017724a85a789bbdb8c850f517ce6f5cfa7
Mark <mark@digicow.net>
2019-07-18 16:31:03 -0400

    handle page-level auth at App level

--- cardiotracker-ui/src/App.vue
+++ cardiotracker-ui/src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
     <NavBar />
-    <router-view />
+    <template v-if="isAuthorized()">
+      <router-view />
+    </template>
+    <template v-else>
+      <Unauthorized />
+    </template>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
+import Unauthorized from '@/components/auth/Unauthorized.vue';
 export default {
   components: {
-    NavBar
+    NavBar,
+    Unauthorized
   },
-  mounted() {
-    this.$store.dispatch("auth/check");
+  methods: {
+    isAuthorized: function() {
+      if(this.$route.meta.requiresAuth) {
+        return this.$store.getters['auth/isLoggedIn'];
+      }
+      return true;
+    }
   }
 }
 </script>
