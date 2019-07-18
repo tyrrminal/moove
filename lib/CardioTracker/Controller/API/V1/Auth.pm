459ccb3e09ea764583a8dbfbc6a5f23bbafd4d1f
Mark <mark@digicow.net>
2019-07-18 16:29:22 -0400

    adapting to new Session response API

--- lib/CardioTracker/Controller/API/V1/Auth.pm
+++ lib/CardioTracker/Controller/API/V1/Auth.pm
@@ -1,5 +1,6 @@
 package CardioTracker::Controller::API::V1::Auth;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
+use DateTime;
 
 sub login($self) {
   my $c = $self->openapi->valid_input or return;
@@ -10,7 +11,7 @@ sub login($self) {
     $c->session(uid => $u->id);
     return $c->render(
       status  => 200,
-      openapi => $u->to_hash
+      openapi => { user => $u->to_hash, expiration => $c->session_expiration }
     );
   }
 
@@ -25,10 +26,9 @@ sub status($self) {
   
   return $c->render(
     status  => 200,
-    openapi => $self->current_user->to_hash
+    openapi => { user => $self->current_user->to_hash, expiration => $c->session_expiration }
   );
 }
-
 
 sub logout($self) {
   my $c = $self->openapi->valid_input or return;
@@ -36,6 +36,21 @@ sub logout($self) {
   $c->session({uid => undef, expires => 1});
 
   return $c->render(status => 204, openapi => '');
+}
+
+sub session_expiration($self) {
+  my $c = $self->openapi->valid_input or return;
+
+  my $d;
+  if($d = $c->session('expiration')) {
+    $d += time;
+  } elsif($d = $c->session('expires')) {
+    
+  } else {
+    $d = time + $c->app->sessions->default_expiration;
+  }
+
+  return DateTime->from_epoch(epoch => $d, time_zone => 'local')->iso8601;
 }
 
 1;
