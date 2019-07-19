b71901dbf130c3d88f36518e9dbed176e464e3ba
Mark <mark@digicow.net>
2019-07-19 13:25:37 -0400

    restructuring home page to provide app info

---
+++ cardiotracker-ui/src/components/home/DisclosureCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div role="tablist">
+    <b-card no-body class="mb-1">
+      <b-card-header role="tab" header-tag="header" v-b-toggle="name" v-html="$options.filters.prefaceAngle(title)" />
+      <b-collapse :id="name" role="tabpanel">
+        <b-card-body>
+          <b-card-text v-if="text">{{ text }}</b-card-text>
+          <b-list-group v-if="items" flush>
+            <b-list-group-item v-for="(i,index) in items" :key="`${name}-${index}`" >{{ i }}</b-list-group-item>
+          </b-list-group>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    text: String,
+    items: Array,
+    name: String
+  },
+  filters: {
+    prefaceAngle: function(t) {
+      return '&rsaquo; ' + t;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
