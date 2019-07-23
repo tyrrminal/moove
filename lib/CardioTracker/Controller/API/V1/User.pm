64fbbd4dec7a14b967b47960d8bd10a3790c0c1b
Mark <mark@digicow.net>
2019-07-23 13:59:41 -0400

    adding totals filtering out undefined events

--- lib/CardioTracker/Controller/API/V1/User.pm
+++ lib/CardioTracker/Controller/API/V1/User.pm
@@ -10,15 +10,29 @@ sub get_summary($self) {
   if (my $u = $c->model('User')->find_user($user_id)) {
     my @recent = $u->activities->whole->ordered('-desc')->slice(0, 4);
 
+    my @results = $c->model('CumulativeTotal')->search(
+      {
+        user_id => $u->id,
+        year    => {'=', undef}
+      }
+    );
+
+    my $reg = $u->event_registrations;
+    my %e   = (
+      previous => $reg->past->visible_to($c->current_user)->ordered('-desc')->first,
+      next     => $reg->future->visible_to($c->current_user)->ordered('-asc')->first
+    );
+    foreach (keys(%e)) {
+      if (defined($e{$_})) {$e{$_} = $e{$_}->to_hash(complete => $TRUE);}
+      else                 {delete($e{$_});}
+    }
     return $c->render(
       status  => 200,
       openapi => {
         user              => $u->to_hash,
         recent_activities => [map {$_->to_hash} @recent],
-        events            => {
-          previous => $u->event_registrations->past->ordered('-desc')->first->to_hash(complete  => $TRUE),
-          next     => $u->event_registrations->future->ordered('-asc')->first->to_hash(complete => $TRUE)
-        }
+        events            => {%e},
+        totals            => [map {$_->to_hash} @results]
       }
     );
   }
