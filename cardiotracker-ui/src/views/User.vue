4357e6ee89a2948b119e0e14b25f646e74f89bb0
Mark <mark@digicow.net>
2019-07-23 14:00:54 -0400

    adding totals adding username

--- cardiotracker-ui/src/views/User.vue
+++ cardiotracker-ui/src/views/User.vue
@@ -9,13 +9,22 @@
     <b-container v-if="user">
       <b-row>
         <b-col sm="12" class="text-center">
-          <h3>{{ user.person.first_name }} {{ user.person.last_name }}</h3>
+          <h3>{{ user.person.first_name }}<template v-if="user.person.first_name.toLowerCase() !== user.username.toLowerCase()"> "{{ user.username }}"</template> {{ user.person.last_name }}</h3>
         </b-col>
       </b-row>
 
       <b-row>
+        <b-col sm="10" class="text-center">
+          <span class="text-muted">Lifetime: </span><span v-for="t in totals.length" :key="totals[t-1].activity_type.id">{{ totals[t-1].activity_type.description }} {{ totals[t-1].distance | format_distance }}<span v-if="t < totals.length" class="text-muted"> / </span></span>
+        </b-col>
+      </b-row>
+
+      <hr />
+
+      <b-row>
         <b-col sm="4">
-          <h4>Recent Activities</h4>
+          <h4>Activities</h4>
+          <h5>Recent</h5>
           <b-list-group>
             <b-list-group-item v-for="a in activities" :key="a.id" ><timeago :datetime="a.start_time"></timeago> &mdash; {{ a.distance | format_distance }} {{ a.activity_type.description }}</b-list-group-item>
           </b-list-group>
@@ -52,6 +61,7 @@ export default {
       user: null,
       activities: [],
       events: null,
+      totals: [],
       sidebarOps: [
         
       ]
@@ -64,6 +74,7 @@ export default {
         self.user = response.data.user;
         self.activities = response.data.recent_activities;
         self.events = response.data.events;
+        self.totals = response.data.totals;
       })
       .catch(err => self.error = err.response.data.message);
   },
