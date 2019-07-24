fe41ef3cd609cde8feb786cac1ec6e7b7e35b045
Mark <mark@digicow.net>
2019-07-24 10:55:23 -0400

    adding goal support tables

---
+++ lib/CardioTracker/Model/Result/GoalComparator.pm
@@ -0,0 +1,113 @@
+use utf8;
+package CardioTracker::Model::Result::GoalComparator;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+CardioTracker::Model::Result::GoalComparator
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::Relationship::Predicate>
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::InflateColumn::Time>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components(
+  "Relationship::Predicate",
+  "InflateColumn::DateTime",
+  "InflateColumn::Time",
+);
+
+=head1 TABLE: C<goal_comparator>
+
+=cut
+
+__PACKAGE__->table("goal_comparator");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 superlative
+
+  data_type: 'enum'
+  default_value: 'N'
+  extra: {list => ["Y","N"]}
+  is_nullable: 1
+
+=head2 operator
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 45
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "superlative",
+  {
+    data_type => "enum",
+    default_value => "N",
+    extra => { list => ["Y", "N"] },
+    is_nullable => 1,
+  },
+  "operator",
+  { data_type => "varchar", is_nullable => 0, size => 45 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<operator_UNIQUE>
+
+=over 4
+
+=item * L</operator>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("operator_UNIQUE", ["operator"]);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2019-07-24 10:54:51
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wPQg+bJYGB6HwXolboqefw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
---
+++ lib/CardioTracker/Model/Result/GoalSpan.pm
@@ -0,0 +1,99 @@
+use utf8;
+package CardioTracker::Model::Result::GoalSpan;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+CardioTracker::Model::Result::GoalSpan
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::Relationship::Predicate>
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::InflateColumn::Time>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components(
+  "Relationship::Predicate",
+  "InflateColumn::DateTime",
+  "InflateColumn::Time",
+);
+
+=head1 TABLE: C<goal_span>
+
+=cut
+
+__PACKAGE__->table("goal_span");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 description
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 45
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "description",
+  { data_type => "varchar", is_nullable => 0, size => 45 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<description_UNIQUE>
+
+=over 4
+
+=item * L</description>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("description_UNIQUE", ["description"]);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2019-07-24 10:54:51
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:QNiQMsNxQ3Yla2gc1udPmQ
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
