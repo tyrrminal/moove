3bd97b760d7efaf14eefcbbba5a0278e76833de4
Mark <mark@digicow.net>
2019-08-02 13:29:21 -0400

    outputing PRs and achievements separately

--- lib/CardioTracker/Command/recalculate_goals.pm
+++ lib/CardioTracker/Command/recalculate_goals.pm
@@ -9,40 +9,43 @@ use CardioTracker::Import::Helper::TextBalancedFix;
 use DCS::Constants qw(:boolean);
 
 has 'description' => 'Recalculate goal activities for a user';
-has 'usage' => <<"USAGE";
+has 'usage'       => <<"USAGE";
 $0 --user [id|name]
 OPTIONS:
   --user      user_id or username to recalc goals for
 USAGE
 
-sub run($self, @args) {
-  getopt(
-    \@args,
-    'user=s' => \(my $user_id),
-  );
+sub run ($self, @args) {
+  getopt(\@args, 'user=s' => \(my $user_id),);
 
   my $user = $self->app->model('User')->find_user($user_id);
 
   my @goals = $self->app->model('UserGoal')->for_user($user);
 
   print "--- PRs ---\n";
-  foreach my $goal (grep { $_->is_pr } @goals) {
+  foreach my $goal (grep {$_->is_pr} @goals) {
     $goal->recalculate();
-        
+
     print $goal->goal->name;
-    print ": ---" and next unless($goal->is_fulfilled);
+    print ": ---" and next unless ($goal->is_fulfilled);
     print "\n";
     foreach ($goal->history) {
-      print "  ". $_->get_goal_description .' (' . $_->date->strftime('%F') . ")\n";
+      print "  " . $_->get_goal_description . ' (' . $_->date->strftime('%F') . ")\n";
     }
   }
 
-  print "--- Achievements ---\n";
-  foreach my $goal (grep { !$_->is_pr } @goals) {
+  print "\n--- Achievements ---\n";
+  foreach my $goal (grep {!$_->is_pr} @goals) {
     $goal->recalculate();
-    print "---------------------------\n";  
 
-    print $goal->goal->name .": ".( $goal->is_fulfilled ? ($goal->get_goal_description .' (' . $goal->user_goal_fulfillments->most_recent->date->strftime('%F') . ")") : "---") . "\n";
+    print $goal->goal->name . ":";
+    if ($goal->is_fulfilled) {
+      print "\n";
+      print "  " . $goal->get_goal_description . ' (' . $goal->user_goal_fulfillments->most_recent->date->strftime('%F') . ")";
+    } else {
+      print " ---";
+    }
+    print "\n";
   }
 }
 
