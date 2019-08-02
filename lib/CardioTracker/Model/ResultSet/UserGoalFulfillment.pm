17b36dae3db04116d71c17df53098494129c8158
Mark <mark@digicow.net>
2019-08-02 13:32:30 -0400

    adding direction opion to ordered accessor

--- lib/CardioTracker/Model/ResultSet/UserGoalFulfillment.pm
+++ lib/CardioTracker/Model/ResultSet/UserGoalFulfillment.pm
@@ -4,18 +4,25 @@ use base qw(DBIx::Class::ResultSet);
 
 sub ordered {
   my $self = shift;
+  my ($direction) = (@_, '-asc');
 
-  $self->search({},{
-    order_by => {'-asc' => 'date'}
-  });
+  $self->search(
+    {},
+    {
+      order_by => {$direction => 'date'}
+    }
+  );
 }
 
 sub most_recent {
   my $self = shift;
 
-  $self->search({},{
-    order_by => {'-desc' => 'date'}
-  })->first;
+  $self->search(
+    {},
+    {
+      order_by => {'-desc' => 'date'}
+    }
+  )->first;
 }
 
 1;
