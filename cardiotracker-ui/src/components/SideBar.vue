ac99680c8da26c6334a2d7faa9035c0f19eb99f7
Mark <mark@digicow.net>
2019-08-11 20:14:08 -0400

    improving index uniqueness removing unused collapse

--- cardiotracker-ui/src/components/SideBar.vue
+++ cardiotracker-ui/src/components/SideBar.vue
@@ -1,10 +1,12 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-collapse is-nav>
-      <b-navbar-nav small>
-        <b-nav-item v-for="(item, index) in items" :key="index" :to="item.to">{{ item.text }}</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
+    <b-navbar-nav small>
+      <b-nav-item
+        v-for="(item, index) in items"
+        :key="`tbitem-${index}`"
+        :to="item.to"
+      >{{ item.text }}</b-nav-item>
+    </b-navbar-nav>
   </b-navbar>
 </template>
 
