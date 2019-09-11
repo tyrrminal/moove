efc7f8058c9caba68cbe8085a8471e07736f491b
Mark <mark@digicow.net>
2019-09-11 14:02:19 -0400

    adding for_event filters

--- lib/CardioTracker/Model/ResultSet/Result.pm
+++ lib/CardioTracker/Model/ResultSet/Result.pm
@@ -4,6 +4,19 @@ use base qw(DBIx::Class::ResultSet);
 
 use DCS::Constants qw(:existence);
 
+sub for_event {
+  my $self = shift;
+  my ($event) = @_;
+
+  return $self->search(
+    {
+      'activity.event_id' => $event->id
+    }, {
+      join => 'activity'
+    }
+  );
+}
+
 sub needs_pace {
   my $self = shift;
 
