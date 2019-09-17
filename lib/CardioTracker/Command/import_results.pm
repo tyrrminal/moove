2d8397bf5672c9c83fefa456d2a7918186362cba
Mark <mark@digicow.net>
2019-09-17 13:31:14 -0400

    correcting db lookup call

--- lib/CardioTracker/Command/import_results.pm
+++ lib/CardioTracker/Command/import_results.pm
@@ -75,7 +75,7 @@ sub import_event ($self, $import_class, $id, $race) {
     }
     my ($result, $user_id);
     if ($person->users->count) {
-      my $act_rs = $person->users->first->activities->find({event_id => $event->id});
+      my $act_rs = $person->users->first->activities->search({event_id => $event->id});
       if ($act_rs->count) {
         $result  = $act_rs->first->result;
         $user_id = $person->users->first->id;
