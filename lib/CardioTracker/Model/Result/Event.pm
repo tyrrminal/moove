3fb8c6f21af80fe7dbb38ba41c3c7cbe3eab0c14
Mark <mark@digicow.net>
2019-09-17 13:54:05 -0400

    removing unnecessary and erroneous check

--- lib/CardioTracker/Model/Result/Event.pm
+++ lib/CardioTracker/Model/Result/Event.pm
@@ -259,11 +259,9 @@ sub update_missing_group_counts {
 sub add_missing_gender_groups {
   my $self = shift;
 
-  if ($self->is_missing_gender_group) {
-    foreach my $g ($self->result_source->schema->resultset('Gender')->all) {
-      $self->create_gender_result_group($g)
-        unless ($self->event_result_groups->search({gender_id => $g->id, division_id => $NULL})->count);
-    }
+  foreach my $g ($self->result_source->schema->resultset('Gender')->all) {
+    $self->create_gender_result_group($g)
+      unless ($self->event_result_groups->search({gender_id => $g->id, division_id => $NULL})->count);
   }
 }
 
