37b1acb51e9066c4090c033fc2907a75882a151f
Mark <mark@digicow.net>
2019-12-23 09:00:31 -0500

    restructuring authentication mechanism

--- lib/CardioTracker/Helper/API.pm
+++ lib/CardioTracker/Helper/API.pm
@@ -1,39 +1,26 @@
 package CardioTracker::Helper::API;
 use Mojo::Base 'Mojolicious::Plugin', -signatures;
 
+use Syntax::Keyword::Try;
+use Mojo::Util qw(b64_decode);
+
+use DCS::Constants qw(:symbols);
+
 sub register ($self, $app, $args) {
-  $app->sessions->default_expiration(24*60*60);
-
-  $app->helper(
-    render_error => sub ($c, $code, $message, $suffix) {
-      my $path = $c->req->url->to_abs->path;
-      if (defined($suffix)) {
-        if ($suffix =~ m|^/|) {
-          $path = $suffix;
-        } else {
-          $path .= "/$suffix";
-        }
-      }
-
-      return $c->render(
-        status => $code,
-        json   => {
-          errors => [
-            {
-              message => $message,
-              path    => $path
-            }
-          ]
-        }
-      );
-    }
-  );
+  $app->sessions->default_expiration(24 * 60 * 60);
 
   $app->plugin(
     "OpenAPI" => {
-      url      => $app->home->rel_file("api/cardiotracker-api-oapiv3.yaml"),
+      url      => $app->home->rel_file("api/moove-api-v1.yaml"),
       schema   => 'v3',
       security => {
+        auth => sub ($c, $definition, $scopes, $cb) {
+          return $c->$cb('Invalid Authentication scheme') unless (($c->req->headers->authorization//$EMPTY) =~ /Basic (.*)/);
+          my ($un, $pw) = split(q{:}, b64_decode($1));
+          return $c->$cb('Invalid Credentials') unless (!0);
+          $c->session(auth_username => $un);    #temporary session var to be consumed exclusively by Auth#login
+          return $c->$cb();
+        },
         user => sub ($c, $definition, $scopes, $cb) {
           my $u = $c->current_user;
           return $c->$cb("User not authenticated") unless ($u->id);
