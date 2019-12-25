d9209814dd0dee6f1a1667d00604770725adab96
Mark <mark@digicow.net>
2019-12-25 12:58:31 -0500

    adding DB retrieve by ID helper

--- lib/Moove/Helper/DB.pm
+++ lib/Moove/Helper/DB.pm
@@ -5,18 +5,27 @@ use Moove::Model;
 
 use Data::Dumper;
 
-sub register($self, $app, $args) {
+sub register ($self, $app, $args) {
 
   $app->helper(
     db => sub($c) {
       my $cfg = $app->conf->db;
-      state $db = Moove::Model->new($cfg->dsn,$cfg->user,$cfg->pass);
+      state $db = Moove::Model->new($cfg->dsn, $cfg->user, $cfg->pass);
     }
   );
 
   $app->helper(
-    model => sub($self, $model) {
+    model => sub ($self, $model) {
       return $self->db->resultset($model);
+    }
+  );
+
+  $app->helper(
+    retrieve => sub ($self, $model, $id) {
+      if ($id and my $m = $self->model($model)) {
+        return $m->find($id);
+      }
+      return undef;
     }
   );
 }
