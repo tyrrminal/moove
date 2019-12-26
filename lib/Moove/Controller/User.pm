7303c2694b7d8468ed4f716a75194e24e40a9442
Mark <mark@digicow.net>
2019-12-25 19:12:48 -0500

    refactoring methods to more appropriate classes

--- lib/Moove/Controller/User.pm
+++ lib/Moove/Controller/User.pm
@@ -7,86 +7,31 @@ sub get_summary($self) {
   my $c = $self->openapi->valid_input or return;
   my $user_id = $c->validation->param('user');
 
-  if (my $u = $c->model('User')->find_user($user_id)) {
-    my @recent = $u->activities->whole->ordered('-desc')->slice(0, 4);
+  my $u = $c->retrieve('User', $user_id) or return $self->render_not_found("User '$user_id' unknown");
 
-    my @results = $c->model('CumulativeTotal')->search(
-      {
-        user_id => $u->id,
-        year    => {'=', undef}
-      }
-    );
+  my @recent = $u->activities->whole->ordered('-desc')->slice(0, 4);
 
-    my $reg = $u->event_registrations;
-    my %e   = (
-      previous => $reg->past->visible_to($c->current_user)->ordered('-desc')->first,
-      next     => $reg->future->visible_to($c->current_user)->ordered('-asc')->first
-    );
-    foreach (keys(%e)) {
-      if (defined($e{$_})) {$e{$_} = $e{$_}->to_hash_complete;}
-      else                 {delete($e{$_});}
-    }
-    return $c->render(
-      status  => 200,
-      openapi => {
-        user              => $u->to_hash,
-        recent_activities => [map {$_->to_hash} @recent],
-        events            => {%e},
-        goals             => [map {$_->to_hash(fulfillments => $TRUE)} $u->user_goals->fulfilled('-desc')->slice(0, 4)],
-        totals => [map {$_->to_hash} @results]
-      }
-    );
-  }
+  my @results = $c->model('CumulativeTotal')->overall_results;
 
-  return $c->render(
-    status  => 404,
-    openapi => {message => "User '$user_id' unknown"}
+  my $reg = $u->event_registrations;
+  my %e   = (
+    previous => $reg->past->visible_to($c->current_user)->ordered('-desc')->first,
+    next     => $reg->future->visible_to($c->current_user)->ordered('-asc')->first
   );
-}
-
-sub get_goals($self) {
-  my $c = $self->openapi->valid_input or return;
-
-  my $user_id = $c->validation->param('user');
-
-  if (my $u = $c->model('User')->find_user($user_id)) {
-    my $goals = $u->user_goals->search({}, {join => 'goal', order_by => 'goal.activity_type_id, goal.name'});
-    return $c->render(
-      status  => 200,
-      openapi => {
-        personalRecords => [map {$_->to_hash(most_recent_fulfillment => $TRUE)} $goals->personal_records->all],
-        achievements    => [
-          map {
-            $_->to_hash(
-              fulfillments         => $TRUE,
-              most_recent_activity => $TRUE,
-              event                => sub {shift->event_registrations->visible_to($c->current_user) > 0}
-              )
-            } $goals->achievements->all
-        ]
-      }
-    );
+  foreach (keys(%e)) {
+    if (defined($e{$_})) {$e{$_} = $e{$_}->to_hash_complete;}
+    else                 {delete($e{$_});}
   }
-}
-
-sub get_goal($self) {
-  my $c = $self->openapi->valid_input or return;
-
-  my $user_id = $c->validation->param('user');
-  my $goal_id = $c->validation->param('goal');
-
-  if (my $u = $c->model('User')->find_user($user_id)) {
-    my $g = $u->user_goals->search({goal_id => $goal_id})->first->to_hash(
-      fulfillments         => $TRUE,
-      most_recent_activity => $TRUE,
-      event                => sub {shift->event_registrations->visible_to($c->current_user) > 0}
-    );
-
-    return $c->render(
-      status  => 200,
-      openapi => $g
-    );
-  }
+  return $c->render(
+    status  => 200,
+    openapi => {
+      user              => $u->to_hash,
+      recent_activities => [map {$_->to_hash} @recent],
+      events            => {%e},
+      goals             => [map {$_->to_hash(fulfillments => $TRUE)} $u->user_goals->fulfilled('-desc')->slice(0, 4)],
+      totals => [map {$_->to_hash} @results]
+    }
+  );
 }
 
 1;
