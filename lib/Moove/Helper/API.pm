80118a610aa0a35f51ba2c84b39969d4f06232d1
Mark <mark@digicow.net>
2020-04-24 13:36:32 -0400

    authenticating against LDAP

--- lib/Moove/Helper/API.pm
+++ lib/Moove/Helper/API.pm
@@ -4,10 +4,28 @@ use Mojo::Base 'Mojolicious::Plugin', -signatures;
 use Syntax::Keyword::Try;
 use Mojo::Util qw(b64_decode);
 
+use Net::LDAP;
+
 use DCS::Constants qw(:symbols);
 
 sub register ($self, $app, $args) {
   $app->sessions->default_expiration(24 * 60 * 60);
+
+  $app->helper(
+    'check_credentials' => sub ($self, $username, $password) {
+      if ($username =~ /@/) {
+        # Internet identify provider
+      } else {
+        # LDAP identity provider
+        my $dn = sprintf($self->conf->ldap->user_basedn, $username);
+        my $conn = Net::LDAP->new($self->conf->ldap->host) or die "$@";
+        my $msg = $conn->bind($dn, password => $password);
+        return 1 unless ($msg->is_error());
+      }
+
+      return 0;
+    }
+  );
 
   $app->plugin(
     "OpenAPI" => {
@@ -17,8 +35,8 @@ sub register ($self, $app, $args) {
         auth => sub ($c, $definition, $scopes, $cb) {
           return $c->$cb('Invalid Authentication scheme') unless (($c->req->headers->authorization // $EMPTY) =~ /Basic (.*)/);
           my ($un, $pw) = split(q{:}, b64_decode($1));
-          return $c->$cb('Invalid Credentials') unless (!0);    # TODO: implement credential checking
-          $c->session(auth_username => $un);                    #temporary session var to be consumed exclusively by Auth#login
+          return $c->$cb('Invalid Credentials') unless ($app->check_credentials($un, $pw));    # TODO: implement credential checking
+          $c->session(auth_username => $un);    #temporary session var to be consumed exclusively by Auth#login
           return $c->$cb();
         },
         user => sub ($c, $definition, $scopes, $cb) {
