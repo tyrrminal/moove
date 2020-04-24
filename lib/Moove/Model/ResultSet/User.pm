eb52a82b7e88ae2ae645c4a96fa5e7b47f03ec59
Mark <mark@digicow.net>
2020-04-24 14:59:33 -0400

    improving guest user definition

--- lib/Moove/Model/ResultSet/User.pm
+++ lib/Moove/Model/ResultSet/User.pm
@@ -4,12 +4,12 @@ use base qw(DBIx::Class::ResultSet);
 
 sub anonymous {
   my $self = shift;
-  return $self->new_result({id => 0, username => 'guest'});
+  return $self->new_result({id => 0, username => 'guest', person => {id => 0, first_name => 'guest', last_name => 'user'}});
 }
 
 sub find_user {
-  my $self = shift;
-  my ($user_id) = @_;
+  my $self         = shift;
+  my ($user_id)    = @_;
   my $search_field = ($user_id =~ /\D/) ? 'username' : 'id';
 
   return $self->search({$search_field => $user_id})->first;
