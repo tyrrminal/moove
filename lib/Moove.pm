a2212a80debefef2c9f649ca76a343b4fb01a126
Mark <mark@digicow.net>
2020-04-28 15:34:21 -0400

    simplifying with base classes

--- lib/Moove.pm
+++ lib/Moove.pm
@@ -1,29 +1,16 @@
-package Moove v0.2.0;
-use Mojo::Base 'Mojolicious', -signatures;
+package Moove v1.0.0;
+use Mojo::Base 'Mojolicious';
+
+use experimental qw(signatures);
 
 # This method will run once at server start
 sub startup($self) {
-  push @{$self->commands->namespaces}, 'Moove::Command';
-
   $self->plugin(
-    'Config::Structured' => {
-      structure_file => $self->app->home->child('cfg')->child('structure.yml')->to_string,
-      config_file    => $ENV{MOOVE_CONFIG}
-    }
+    'DCS::Base',
+    dbix_components => qw(InflateColumn::Time),
+    api_definition  => $self->app->home->child('api')->child('moove-api-v1.yaml')->to_string,
+    conf_structure  => $self->app->home->child('cfg')->child('structure.yml')->to_string
   );
-  $self->secrets($self->conf->secrets);
-
-  $self->conf->_add_helper(
-    db_params => sub {
-      my $db = shift->db;
-      return (sprintf('dbi:mysql:database=%s;host=%s;port=%d', $db->name, $db->host, $db->port), $db->user, $db->pass);
-    }
-  );
-
-  $self->plugin('Moove::Helper::DB');
-  $self->plugin('Moove::Helper::Session');
-  $self->plugin('Moove::Helper::API');
-  $self->plugin('Moove::Helper::Vue');
 }
 
 1;
