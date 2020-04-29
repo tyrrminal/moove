592eed8d891490fd28deef85c89b787b4c4b89e1
Mark <mark@digicow.net>
2020-04-29 12:08:50 -0400

    implementing meta list API endpoint

---
+++ lib/Moove/Controller/Meta.pm
@@ -0,0 +1,23 @@
+package Moove::Controller::Meta;
+use Mojo::Base 'DCS::API::Base::ModelController';
+use Role::Tiny::With;
+
+use DCS::NameConversion qw(camel_lower_to_upper camel_plural_to_singular);
+
+use experimental qw(signatures);
+
+sub list($self) {
+  return unless $self->openapi->valid_input;
+
+  my $r = {};
+  foreach (qw(activityTypes eventTypes eventReferenceTypes dimensions unitOfMeasure)) {
+    if ($self->validation->param($_) || $self->validation->param('all')) {
+      my $model_name = camel_lower_to_upper(camel_plural_to_singular($_));
+      $r->{$_} = [$self->model($model_name)->all];
+    }
+  }
+
+  return $self->render(openapi => $self->render_model($r));
+}
+
+1;
