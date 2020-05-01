6e523f05ded0834e2c2ea8f46a1e910c71f88244
Mark <mark@digicow.net>
2020-05-01 15:47:20 -0400

    getting signatures via experimental

--- lib/Mojolicious/Plugin/DCS/Vue.pm
+++ lib/Mojolicious/Plugin/DCS/Vue.pm
@@ -1,5 +1,7 @@
 package Mojolicious::Plugin::DCS::Vue;
-use Mojo::Base 'Mojolicious::Plugin', -signatures;
+use Mojo::Base 'Mojolicious::Plugin';
+
+use experimental qw(signatures);
 
 sub register ($self, $app, $args) {
   # Route everything that other routes haven't picked up to Vue
@@ -7,7 +9,7 @@ sub register ($self, $app, $args) {
     '/*catchall' => {catchall => ''} => sub($c) {
       $c->reply->static('index.html');
     }
-    );
+  );
 }
 
 1;
