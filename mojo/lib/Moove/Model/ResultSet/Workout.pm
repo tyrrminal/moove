177e5efb3da738d9f07bce7bc1dbb5a8149fe5df
Mark Tyrrell <mark@digicowsoftware.com>
2021-01-12 22:07:41 -0500

    add date filters for workouts

---
+++ mojo/lib/Moove/Model/ResultSet/Workout.pm
@@ -0,0 +1,24 @@
+package Moove::Model::ResultSet::Workout;
+use base qw(DBIx::Class::ResultSet);
+
+use experimental qw(signatures postderef);
+
+sub after_date ($self, $date) {
+  my $d = ref($date) ? DateTime::Format::MySQL->format_date($date) : $date;
+  return $self->search(
+    {
+      date => {'>=' => $d}
+    }
+  );
+}
+
+sub before_date ($self, $date) {
+  my $d = ref($date) ? DateTime::Format::MySQL->format_date($date) : $date;
+  return $self->search(
+    {
+      date => {'<=' => $d}
+    }
+  );
+}
+
+1;
