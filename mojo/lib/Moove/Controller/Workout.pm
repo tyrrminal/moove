3018c665c753a393d391869a69197564d55a339c
Mark Tyrrell <mark@digicowsoftware.com>
2021-01-12 22:08:50 -0500

    add workout listing

---
+++ mojo/lib/Moove/Controller/Workout.pm
@@ -0,0 +1,26 @@
+package Moove::Controller::Workout;
+use Mojo::Base 'DCS::API::Base::ModelController';
+
+use Role::Tiny::With;
+with 'DCS::API::Role::Rest::Collection';
+with 'Moove::Controller::Role::ModelEncoding::Workout';
+
+use boolean;
+use List::Util qw(sum min max);
+
+use experimental qw(signatures postderef);
+
+sub resultset ($self, @args) {
+  my $rs = $self->SUPER::resultset(@args);
+
+  if (my $start_date = $self->validation->param('start')) {
+    $rs = $rs->after_date($start_date);
+  }
+  if (my $end_date = $self->validation->param('end')) {
+    $rs = $rs->before_date($end_date);
+  }
+
+  return $rs;
+}
+
+1;
