00a679c0e9604794636a59533ff3a0df9fbc0fdb
Mark Tyrrell <mark@digicowsoftware.com>
2021-01-19 16:36:23 -0500

    add stub views

---
+++ vue/src/views/Workouts.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Sidebar :items="menu" />
+  </div>
+</template>
+
+<script>
+import Sidebar from "@/components/SideBar.vue";
+
+export default {
+  components: {
+    Sidebar,
+  },
+  data: function () {
+    return {
+      menu: [{ text: "Summary", to: { name: "activitiesSummary" } }],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
