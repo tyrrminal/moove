3b58e6e2697d2b8e4decc4c3b8b6731a22287ab5
Mark Tyrrell <mark@digicowsoftware.com>
2021-01-19 16:34:48 -0500

    add/remove modules

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,8 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-// Moment (Date formatting)
-Vue.use(require('vue-moment'));
+// Axios (Networking)
+import VueAxios from "vue-axios";
+import axios from './services/axios';
+Vue.use(VueAxios, axios);
+
+// Headful (page titles & meta)
+import vueHeadful from 'vue-headful';
+Vue.component('vue-headful', vueHeadful);
+
+// Luxon (Date formatting)
+import VueLuxon from "vue-luxon"
+Vue.use(VueLuxon);
 
 //String Formatting
 import VuePluralize from 'vue-pluralize';
@@ -13,15 +23,8 @@ Vue.use(VuePluralize);
 //Number Formatting
 import vueNumeralFilterInstaller from 'vue-numeral-filter';
 Vue.use(vueNumeralFilterInstaller);
-
-// Headful (page titles & meta)
-import vueHeadful from 'vue-headful';
-Vue.component('vue-headful', vueHeadful);
-
-// Axios (Networking)
-import VueAxios from "vue-axios";
-import axios from './services/axios';
-Vue.use(VueAxios, axios);
+import Vue2Filters from 'vue2-filters'
+Vue.use(Vue2Filters)
 
 import VueTimeago from 'vue-timeago'
 Vue.use(VueTimeago, {
@@ -30,24 +33,12 @@ Vue.use(VueTimeago, {
 });
 
 // Layout and design
-import BootstrapVue from "bootstrap-vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-
-// Symbols and icons
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
-import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
-import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
-import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons';
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-library.add(faAngleRight, faAngleLeft, faAngleDoubleRight, faAngleDoubleLeft, faExternalLinkAlt, faTimes, faCheck);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+import "bootstrap/dist/js/bootstrap.min.js";
 
 // Vue
 Vue.config.productionTip = false;
