6363d5daba39a115556157af5ee47b0ce9f36c66
Mark Tyrrell <mark@digicowsoftware.com>
2021-01-19 16:31:45 -0500

    use model encoding remove unused models

--- mojo/lib/Moove/Controller/Meta.pm
+++ mojo/lib/Moove/Controller/Meta.pm
@@ -2,6 +2,8 @@ package Moove::Controller::Meta;
 use Mojo::Base 'DCS::API::Base::ModelController';
 use Role::Tiny::With;
 
+with 'Moove::Controller::Role::ModelEncoding::ActivityType', 'Moove::Controller::Role::ModelEncoding::EventType';
+
 use DCS::NameConversion qw(camel_lower_to_upper camel_plural_to_singular);
 
 use experimental qw(signatures);
@@ -10,7 +12,7 @@ sub list($self) {
   return unless $self->openapi->valid_input;
 
   my $r = {};
-  foreach (qw(activityTypes eventTypes eventReferenceTypes dimensions unitOfMeasure)) {
+  foreach (qw(activityTypes eventTypes unitOfMeasure)) {
     if ($self->validation->param($_) || $self->validation->param('all')) {
       my $model_name = camel_lower_to_upper(camel_plural_to_singular($_));
       $r->{$_} = [$self->model($model_name)->all];
