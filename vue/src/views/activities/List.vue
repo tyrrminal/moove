3cdc3360fa5703ccbfe009e8f1ae763a3cfdae64
Mark Tyrrell <mark@digicowsoftware.com>
2021-02-06 12:45:50 -0500

    switch to item provider function

--- vue/src/views/activities/List.vue
+++ vue/src/views/activities/List.vue
@@ -1,23 +1,64 @@
 <template>
   <b-container class="mt-2">
-    <b-row>
-      <b-col>
-        <b-datepicker v-model="internal.start" /> -
-        <b-datepicker v-model="internal.end" />
-      </b-col>
-    </b-row>
-    <b-table :items="activities" :fields="columns" borderless>
+    <b-form inline>
+      <b-datepicker v-model="internal.range.start" reset-button />
+      <b-icon icon="arrow-right" class="mx-2" />
+      <b-datepicker v-model="internal.range.end" reset-button />
+      <b-button variant="secondary" v-b-modal.filters>Filters</b-button>
+    </b-form>
+    <b-table
+      id="activitiesListTable"
+      class="mt-2"
+      borderless
+      :items="getData"
+      :fields="columns"
+      :per-page.sync="page.length"
+      :current-page.sync="page.current"
+    >
+      <template #table-busy>
+        <div class="text-center">
+          <b-spinner variant="secondary" type="grow" />
+        </div>
+      </template>
       <template #cell(start_time)="data">
         {{ data.value | luxon }}
-      </template>
-      <template #cell(distance)="data">
-        {{ data.item.distance.value }}
-        {{ getUnitOfMeasure(data.item.distance.unitOfMeasureID).abbreviation }}
       </template>
       <template #cell(time)="data">
         {{ data.item.net_time }}
       </template>
+      <template #cell(activityType)="data">
+        {{ getActivityType(data.item.activityTypeID).description }}
+      </template>
+      <template #cell(distance)="data">
+        <template v-if="data.value">
+          {{ data.value.value | number("0.00") }}
+          {{ getUnitOfMeasure(data.value.unitOfMeasureID).abbreviation }}
+        </template>
+      </template>
+      <template #cell(pace)="data">
+        <template v-if="data.value">
+          {{ data.value.value | trimTime
+          }}{{ getUnitOfMeasure(data.value.unitOfMeasureID).abbreviation }}
+        </template>
+      </template>
+      <template #cell(speed)="data">
+        <template v-if="data.value">
+          {{ data.value.value | number("0.0") }}
+          {{ getUnitOfMeasure(data.value.unitOfMeasureID).abbreviation }}
+        </template>
+      </template>
     </b-table>
+    <b-pagination
+      v-model="page.current"
+      :per-page="page.length"
+      :total-rows="total"
+    />
+
+    <b-modal id="filters">
+      <b-checkbox v-model="internal.whole"
+        >Combine Partial Activities</b-checkbox
+      >
+    </b-modal>
   </b-container>
 </template>
 
@@ -28,30 +69,21 @@ export default {
   name: "ActivitiesList",
   data() {
     return {
-      activities: [],
-      sort: {
-        "order.by": "start_time",
-        "order.dir": "asc",
+      watchInternal: false,
+      activityType: null,
+      total: 0,
+      page: {
+        length: 10,
+        current: 1,
       },
       internal: {
         activityTypeID: null,
-        start: null,
-        end: null,
+        range: {
+          start: null,
+          end: null,
+        },
+        whole: true,
       },
-      columns: [
-        {
-          key: "start_time",
-        },
-        {
-          key: "distance",
-        },
-        {
-          key: "time",
-        },
-        {
-          key: "pace",
-        },
-      ],
     };
   },
   props: {
@@ -68,21 +100,38 @@ export default {
   mounted() {
     this.internal.activityTypeID =
       this.$route.query.activityTypeID || this.activityTypeID;
-    this.internal.start = this.$route.query.start || this.start;
-    this.internal.end = this.$route.query.end || this.end;
+    this.internal.range.start = this.$route.query.start || this.start;
+    this.internal.range.end = this.$route.query.end || this.end;
 
     if (this.internal.start != null) this.sort["order.dir"] = "asc";
     if (this.internal.end != null) this.sort["order.dir"] = "desc";
 
-    this.getData();
+    this.$nextTick(function () {
+      this.watchInternal = true;
+    });
+  },
+  filters: {
+    trimTime: function (t) {
+      if (t == null) return t;
+      return t.replace(/^[0:]*/, "");
+    },
   },
   methods: {
-    getData: function () {
+    getData: function (ctx, callback) {
       let self = this;
       this.$http
-        .get("activities", { params: this.queryParams })
+        .get("activities", {
+          params: {
+            ...this.queryParams,
+            "order.by": ctx.sortBy || "start_time",
+            "order.dir": ctx.sortDesc ? "desc" : "asc",
+            "page.number": ctx.currentPage,
+            "page.length": ctx.perPage,
+          },
+        })
         .then((resp) => {
-          self.activities = resp.data.elements;
+          callback(resp.data.elements);
+          self.total = resp.data.pagination.counts.filter;
         });
     },
   },
@@ -92,14 +141,75 @@ export default {
       getUnitOfMeasure: "getUnitOfMeasure",
       isLoaded: "isLoaded",
     }),
+    dateRange: function () {
+      return this.internal.range;
+    },
+    columns: function () {
+      let r = [
+        {
+          key: "start_time",
+          sortable: true,
+          label: "Start",
+        },
+      ];
+      let a = this.getActivityType(this.internal.activityTypeID);
+      if (a == null) {
+        r.push({
+          key: "activityType",
+          sortable: true,
+          label: "Type",
+        });
+      }
+      if (a == null || a.hasDistance) {
+        r.push({
+          key: "distance",
+          sortable: true,
+        });
+      }
+      if (a == null || a.hasDuration) {
+        r.push({
+          key: "time",
+          sortable: true,
+        });
+      }
+      if (a == null || a.hasPace) {
+        r.push({
+          key: "pace",
+          sortable: true,
+        });
+      }
+      if (a == null || a.hasSpeed) {
+        r.push({
+          key: "speed",
+          sortable: true,
+        });
+      }
+      return r;
+    },
     queryParams: function () {
       let r = {
         ...this.sort,
+        combine: this.internal.whole,
+        "page.length": 0,
       };
-      ["activityTypeID", "start", "end"].forEach((k) => {
+      ["activityTypeID"].forEach((k) => {
         if (this.internal[k] != null) r[k] = this.internal[k];
       });
+      ["start", "end"].forEach((k) => {
+        if (this.internal.range[k]) r[k] = this.internal.range[k];
+      });
       return r;
+    },
+  },
+  watch: {
+    internal: {
+      deep: true,
+      handler: function () {
+        if (this.watchInternal)
+          if (Object.keys(this.$route.query).length)
+            this.$router.push({ query: {} });
+        this.$root.$emit("bv::refresh::table", "activitiesListTable");
+      },
     },
   },
 };
