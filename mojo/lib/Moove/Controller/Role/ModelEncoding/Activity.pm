94074607555da2f8c2e7b59cf257cb4bebd724d7
Mark Tyrrell <mark@digicowsoftware.com>
2021-02-06 12:43:23 -0500

    include units with value measurements

--- mojo/lib/Moove/Controller/Role/ModelEncoding/Activity.pm
+++ mojo/lib/Moove/Controller/Role/ModelEncoding/Activity.pm
@@ -30,15 +30,20 @@ sub encode_model_result ($self, $type, $entity) {
     temperature => $entity->temperature,
   };
   if ($base->has_distance) {
-    $r->{distance} = $self->encode_model($entity->distance);
+    $r->{distance} = $self->encode_value_with_units($entity->distance->value, $entity->distance->unit_of_measure);
   }
   if ($base->has_duration) {
     $r->{duration} = $self->encode_time($entity->duration);
   }
-  if ($base->has_distance && $base->has_duration) {
+  if ($base->has_speed) {
     $r->{net_time} = $self->encode_time($entity->net_time);
-    $r->{pace}     = $self->encode_time($entity->pace);
-    $r->{speed}    = $entity->speed;
+    $r->{speed} =
+      $self->encode_value_with_units($entity->speed, $self->model('UnitOfMeasure')->find({abbreviation => 'mph'}));
+  }
+  if ($base->has_pace) {
+    $r->{net_time} = $self->encode_time($entity->net_time);
+    $r->{pace}     = $self->encode_value_with_units($self->encode_time($entity->pace),
+      $self->model('UnitOfMeasure')->find({abbreviation => '/mi'}));
   }
   if ($base->has_repeats) {
     $r->{repetitions} = $entity->repetitions;
@@ -47,6 +52,13 @@ sub encode_model_result ($self, $type, $entity) {
     $r->{mapVisibilityTypeID} = $entity->map_visibility_type_id;
   }
   return $r;
+}
+
+sub encode_value_with_units ($self, $value, $unit) {
+  return {
+    value           => $value,
+    unitOfMeasureID => $unit->id,
+  };
 }
 
 1;
