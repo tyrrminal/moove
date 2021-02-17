fc20372a5324c614b55cb407f8f8a67de345a37b
Mark Tyrrell <mark@digicowsoftware.com>
2021-02-17 16:16:34 -0500

    expose partial descriptors for activities

--- mojo/lib/Moove/Controller/Role/ModelEncoding/ActivityType.pm
+++ mojo/lib/Moove/Controller/Role/ModelEncoding/ActivityType.pm
@@ -15,6 +15,10 @@ sub encode_model_activitytype ($self, $entity) {
     hasPace     => $self->encode_boolean($entity->base_activity_type->has_pace),
     hasSpeed    => $self->encode_boolean($entity->base_activity_type->has_speed),
     hasMap      => $self->encode_boolean($entity->activity_context ? $entity->activity_context->has_map : 0),
+    labels      => {
+      base => $entity->base_activity_type->description,
+      context => (defined($entity->activity_context) ? $entity->activity_context->description : undef),
+    }
   };
 }
 
