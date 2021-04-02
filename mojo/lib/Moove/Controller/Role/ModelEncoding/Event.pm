641e314168833990260536aa97bb8edf603d0c2b
Mark Tyrrell <mark@digicowsoftware.com>
2021-04-02 11:02:51 -0400

    redo event encoding for new schema

--- mojo/lib/Moove/Controller/Role/ModelEncoding/Event.pm
+++ mojo/lib/Moove/Controller/Role/ModelEncoding/Event.pm
@@ -3,5 +3,16 @@ use Role::Tiny;
 
 use experimental qw(signatures);
 
+sub encode_model_event ($self, $event) {
+  return {
+    id => $event->id,
+    name => $event->name,
+    year => $event->year,
+    address => $self->encode_model($event->address),
+    activities => $self->encode_model([$event->event_activities]),
+    defined($event->event_group) ? (event_group => $self->encode_model($event->event_group)) : (),
+    event_series => $self->encode_model([$event->event_groups]),
+  };
+}
 
 1;
