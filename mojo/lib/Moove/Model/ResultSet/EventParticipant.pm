ac202f41ca7771f8cf90d78e6e413d0bf31f8cfe
Mark Tyrrell <mark@digicowsoftware.com>
2021-04-05 16:30:30 -0400

    add accessors

---
+++ mojo/lib/Moove/Model/ResultSet/EventParticipant.pm
@@ -0,0 +1,35 @@
+package Moove::Model::ResultSet::EventParticipant;
+use strict;
+use warnings;
+
+use base qw(DBIx::Class::ResultSet);
+
+use experimental qw(signatures postderef);
+
+sub for_event_activity ($self, $ea) {
+  return $self->search(
+    {
+      'event_registration.event_activity_id' => $ea->id,
+    }, {
+      join => 'event_registration'
+    }
+  );
+}
+
+sub in_division ($self, $division) {
+  return $self->search(
+    {
+      division_id => $division->id
+    }
+  );
+}
+
+sub of_gender ($self, $gender) {
+  return $self->search(
+    {
+      gender_id => $gender->id
+    }
+  );
+}
+
+1;
