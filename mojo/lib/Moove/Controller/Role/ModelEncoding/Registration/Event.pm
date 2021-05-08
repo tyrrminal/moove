dd65c189b016dcd4ad5bbf6fdb06fe289e7e464c
Mark Tyrrell <mark@digicowsoftware.com>
2021-05-08 09:11:10 -0400

    use camelCase for API

--- mojo/lib/Moove/Controller/Role/ModelEncoding/Registration/Event.pm
+++ mojo/lib/Moove/Controller/Role/ModelEncoding/Registration/Event.pm
@@ -5,12 +5,12 @@ use experimental qw(signatures);
 
 sub encode_model_event ($self, $event) {
   return {
-    id => $event->id,
-    name => $event->name,
-    year => $event->year,
+    id      => $event->id,
+    name    => $event->name,
+    year    => $event->year,
     address => $self->encode_model($event->address),
-    defined($event->event_group) ? (event_group => $self->encode_model($event->event_group)) : (),
-    event_series => $self->encode_model([$event->event_groups]),
+    defined($event->event_group) ? (eventGroup => $self->encode_model($event->event_group)) : (),
+    eventSeries => $self->encode_model([$event->event_groups]),
   };
 }
 
