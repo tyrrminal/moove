15ea5bf5917a8bc25c7840a0c5918f4b3606a2a1
Mark Tyrrell <mark@digicowsoftware.com>
2021-05-10 21:52:40 -0400

    rewrite event details view add donation adding

---
+++ vue/src/components/event/fundraising/AddDonation.vue
@@ -0,0 +1,350 @@
+<template>
+  <b-modal
+    id="addDonation"
+    title="Add Donation"
+    @show="onShow"
+    @ok="save"
+    no-close-on-backdrop
+  >
+    <b-breadcrumb>
+      <b-breadcrumb-item
+        text="Donor"
+        @click="clearDonor"
+        :active="stageDonor"
+      />
+      <b-breadcrumb-item
+        text="Address"
+        @click="clearAddress"
+        :active="stageAddress"
+        v-if="stageAddress || stageAmount"
+      />
+      <b-breadcrumb-item
+        text="Amount"
+        v-if="stageAmount"
+        :active="stageAmount"
+      />
+    </b-breadcrumb>
+    <template v-if="stageDonor">
+      <b-form inline @submit.prevent="searchDonors">
+        <b-form-group label="Firstname" class="mr-2">
+          <b-input v-model="search.firstname" size="sm" />
+        </b-form-group>
+        <b-form-group label="Lastname" class="mr-2">
+          <b-input v-model="search.lastname" size="sm" />
+        </b-form-group>
+        <b-button type="submit" variant="primary" size="sm" class="mt-4"
+          >Search</b-button
+        >
+      </b-form>
+      <template v-if="results.donors">
+        <hr />
+        <template v-if="results.donors.length">
+          <b-form-group label="Donors">
+            <b-input-group>
+              <b-select :options="donorSearchResults" v-model="preselect.donor">
+              </b-select>
+              <template #append>
+                <b-button
+                  @click="selectDonor"
+                  :disabled="preselect.donor == null"
+                  variant="success"
+                  >Select</b-button
+                >
+              </template>
+            </b-input-group>
+          </b-form-group>
+          <div class="text-center">or</div>
+        </template>
+        <h4 class="text-center" v-else>No results</h4>
+        <b-row align-h="center">
+          <b-button
+            :disabled="!fullName"
+            class="mt-1"
+            size="sm"
+            variant="success"
+            @click="createNewDonor"
+            >Create New</b-button
+          >
+        </b-row>
+      </template>
+    </template>
+    <template v-else-if="stageAddress">
+      <b-row>
+        <b-col>
+          <b-form-group label="Firstname" label-size="sm">
+            <b-input
+              :disabled="true"
+              v-model="selected.person.firstname"
+              size="sm"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group label="Lastname" label-size="sm">
+            <b-input
+              :disabled="true"
+              v-model="selected.person.lastname"
+              size="sm"
+            />
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row v-if="results.addresses.length > 0">
+        <b-col>
+          <b-input-group>
+            <b-select :options="donorAddresses" v-model="preselect.address" />
+            <template #append>
+              <b-button
+                variant="success"
+                :disabled="preselect.address == null"
+                @click="selectAddress"
+                >Select</b-button
+              >
+            </template>
+          </b-input-group>
+        </b-col>
+      </b-row>
+      <div class="text-center">or</div>
+      <b-row>
+        <b-col>
+          <b-form-group label="Street 1">
+            <b-input v-model="preselect.newAddress.street1" />
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group label="Street 2">
+            <b-input v-model="preselect.newAddress.street2" />
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="6">
+          <b-form-group label="City">
+            <b-input v-model="preselect.newAddress.city" />
+          </b-form-group>
+        </b-col>
+        <b-col cols="2">
+          <b-form-group label="State">
+            <b-input v-model="preselect.newAddress.state" />
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group label="Zip">
+            <b-input v-model="preselect.newAddress.zip" />
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="4">
+          <b-form-group label="Phone #">
+            <b-input v-model="preselect.newAddress.phone" />
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group label="Email Address">
+            <b-input v-model="preselect.newAddress.email" />
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row align-h="center">
+        <b-button variant="success" @click="createNewAddress" size="sm"
+          >Create New</b-button
+        >
+      </b-row>
+    </template>
+    <template v-else-if="stageAmount">
+      <b-form-group label="Donor">
+        <b-input
+          :value="`${selected.person.firstname} ${selected.person.lastname}`"
+          :disabled="true"
+        />
+      </b-form-group>
+      <b-form-group label="Address">
+        <b-input
+          :value="$options.filters.formatAddress(selected.address)"
+          :disabled="true"
+        ></b-input>
+      </b-form-group>
+
+      <b-form-group label="Date">
+        <b-datepicker v-model="selected.date" />
+      </b-form-group>
+      <b-form-group label="Amount">
+        <b-input-group prepend="$">
+          <b-input v-model="selected.amount" />
+        </b-input-group>
+      </b-form-group>
+    </template>
+  </b-modal>
+</template>
+
+<script>
+import { DateTime } from "luxon";
+import EventFilters from "@/mixins/EventFilters.js";
+
+export default {
+  mixins: [EventFilters],
+  props: {
+    userEventActivityID: {
+      type: Number,
+      required: true,
+    },
+  },
+  data: function () {
+    return {
+      search: {
+        firstname: "",
+        lastname: "",
+        results: null,
+      },
+      results: {
+        donors: null,
+        addresses: null,
+      },
+      preselect: {
+        donor: null,
+        address: null,
+        newAddress: {
+          street1: null,
+          street2: null,
+          city: null,
+          state: null,
+          country: null,
+          zip: null,
+          phone: null,
+          email: null,
+        },
+      },
+      selected: {
+        person: null,
+        address: null,
+        date: null,
+        amount: null,
+      },
+    };
+  },
+  methods: {
+    save: function (bvModalEvent) {
+      this.$http
+        .post(
+          ["user", "events", this.userEventActivityID, "donations"].join("/"),
+          this.selected
+        )
+        .then((resp) => {
+          this.$root.$emit("bv::hide::modal", "addDonation");
+          this.$emit("update", resp.data);
+        });
+      bvModalEvent.preventDefault();
+    },
+    onShow: function () {
+      this.search = { firstname: "", lastname: "" };
+      this.results = { donors: null, addresses: null };
+      this.preselect = {
+        donor: null,
+        newAddress: {
+          street1: null,
+          street2: null,
+          city: null,
+          state: null,
+          country: null,
+          zip: null,
+          phone: null,
+          email: null,
+        },
+      };
+      this.selected = {
+        person: null,
+        address: null,
+        date: DateTime.local().toISODate(),
+        amount: null,
+      };
+    },
+    searchDonors: function () {
+      let q = {};
+      ["firstname", "lastname"].forEach((x) => {
+        if (this.search[x]) q[x] = this.search[x];
+      });
+      this.$http
+        .get("donors", {
+          params: q,
+        })
+        .then(
+          (resp) =>
+            (this.results.donors = resp.data.map((x) => ({
+              ...x.person,
+              addresses: x.addresses,
+            })))
+        );
+    },
+    clearAddress: function () {
+      this.selected.address = null;
+    },
+    clearDonor: function () {
+      this.selected.person = null;
+    },
+    selectDonor: function () {
+      this.selected.person = {
+        ...this.results.donors.find((x) => x.id == this.preselect.donor),
+      };
+      this.results.addresses = this.selected.person.addresses;
+      delete this.selected.person.addresses;
+    },
+    createNewDonor: function () {
+      this.selected.person = {
+        firstname: this.search.firstname,
+        lastname: this.search.lastname,
+      };
+      this.results.addresses = [];
+    },
+    selectAddress: function () {
+      this.selected.address = this.results.addresses.find(
+        (x) => x.id == this.preselect.address
+      );
+    },
+    createNewAddress: function () {
+      this.selected.address = this.preselect.newAddress;
+    },
+  },
+  computed: {
+    stageDonor: function () {
+      return this.selected.person == null;
+    },
+    stageAddress: function () {
+      return this.selected.person != null && this.selected.address == null;
+    },
+    stageAmount: function () {
+      return this.selected.person != null && this.selected.address != null;
+    },
+
+    donorSearchResults: function () {
+      return this.results.donors.map((x) => ({
+        ...x,
+        text: `${x.firstname} ${x.lastname}`,
+        value: x.id,
+      }));
+    },
+    donorAddresses: function () {
+      let self = this;
+      return this.results.addresses.map((x) => ({
+        ...x,
+        text: self.$options.filters.formatAddress(x),
+        value: x.id,
+      }));
+    },
+    donorSelected: function () {
+      return this.selected.person != null;
+    },
+    addressSelected: function () {
+      return this.selected.address != null;
+    },
+    fullName: function () {
+      return (
+        this.search.firstname.length > 1 && this.search.lastname.length > 1
+      );
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- vue/src/views/events/Detail.vue
+++ vue/src/views/events/Detail.vue
@@ -1,83 +1,240 @@
 <template>
-  <layout-default>
-    <template #sidebar>
-      <SideBar :items="sidebarOps" />
-    </template>
+  <b-container v-if="userEventActivity">
+    <b-card no-body class="mt-3 mb-3">
+      <b-card-header
+        ><h2>{{ event.year }} {{ event.name }}</h2></b-card-header
+      >
+      <b-card-body>
+        <b-row>
+          <b-col cols="6">
+            <b-jumbotron class="py-2 event-details" border-variant="primary">
+              <b-form-group label="What">
+                {{ Number.parseFloat(eventActivity.distance.value) }}
+                {{ uom(eventActivity.distance.unitOfMeasureID).abbreviation }}
+                {{ eventActivity.eventType.description }}
+              </b-form-group>
+              <b-form-group label="When">
+                <b-icon icon="calendar" class="mr-1" />{{
+                  eventActivity.scheduledStart
+                    | luxon({ input: { zone: "local" }, output: "short" })
+                }}
+              </b-form-group>
+              <b-form-group label="Where">
+                <b-icon icon="compass" class="mr-1" />{{
+                  event.address | formatAddress
+                }}
+              </b-form-group>
+              <b-form-group
+                label="Website"
+                v-if="eventGroup != null && eventGroup.url"
+              >
+                <b-link :href="eventGroup.url" target="_blank">{{
+                  eventGroup.url
+                }}</b-link>
+              </b-form-group>
+            </b-jumbotron>
+          </b-col>
+          <b-col>
+            <b-jumbotron class="py-2 event-details" border-variant="info">
+              <b-form-group
+                label="Registered On"
+                v-if="userEventActivity.date_registered"
+              >
+                {{
+                  userEventActivity.date_registered
+                    | luxon({ input: { zone: "local" }, output: "date_med" })
+                }}
+              </b-form-group>
+              <b-form-group
+                label="Total Fee"
+                v-if="userEventActivity.fee != null"
+              >
+                {{ userEventActivity.fee | currency }}
+              </b-form-group>
+              <b-form-group
+                label="Registration Number"
+                v-if="userEventActivity.registration_number != null"
+              >
+                #{{ userEventActivity.registration_number }}
+              </b-form-group>
+            </b-jumbotron>
+          </b-col>
+        </b-row>
+      </b-card-body>
+    </b-card>
 
-    <b-container v-if="event">
-      <b-row>
-        <b-col sm="1">
-          <Links :links="links" :user="effectiveUser" direction="prev" />
-        </b-col>
-        <b-col sm="10" md="7" lg="5" class="mx-auto">
-          <RegistrationStatus
-            :registration="event.registration"
-            :isInFuture="eventIsInFuture"
-          />
-          <EventDetails
-            :event="event.event"
-            :isPublic="event.registration.is_public"
-          />
-        </b-col>
-        <b-col sm="1">
-          <Links :links="links" :user="effectiveUser" direction="next" />
-        </b-col>
-      </b-row>
+    <b-card no-body class="mb-3" v-if="eventIsInFuture">
+      <b-card-header><h3>Countdown</h3></b-card-header>
+      <b-card-body>
+        <Countdown :end="countdownTarget" />
+      </b-card-body>
+    </b-card>
 
-      <b-row v-if="sequence && sequence.length > 1">
-        <EventSequence
-          class="mx-auto"
-          :id="event.event.event_sequence_id"
-          :events="sequence"
-          :current="event.event.id"
-        />
-      </b-row>
+    <b-card no-body v-if="activity" class="mb-3">
+      <b-card-header><h3>Results</h3></b-card-header>
+      <b-card-body>
+        <b-jumbotron class="p-2" v-if="activity.note">
+          <em>{{ activity.note }}</em>
+        </b-jumbotron>
+        <b-row>
+          <b-col cols="4">
+            <b-jumbotron class="event-details py-2" border-variant="secondary">
+              <b-form-group label="Measured Distance">
+                {{ fillUnits(activity.distance) | formatDistance }}
+              </b-form-group>
+              <b-form-group label="Total Time" v-if="activity.duration">
+                {{ activity.duration }}
+              </b-form-group>
+              <b-form-group
+                label="Net Time"
+                v-if="
+                  activity.net_time && activity.duration != activity.net_time
+                "
+              >
+                {{ activity.net_time }}
+              </b-form-group>
+              <b-form-group label="Avg. Pace" v-if="activity.pace">
+                {{ fillUnits(activity.pace) | formatDistance }}
+              </b-form-group>
+              <b-form-group label="Avg. Speed" v-if="activity.speed">
+                {{ fillUnits(activity.speed) | formatDistance }}
+              </b-form-group>
+              <b-form-group label="Temperature" v-if="activity.temperature">
+                {{ activity.temperature }}° F
+              </b-form-group>
+            </b-jumbotron>
+          </b-col>
 
-      <b-row class="mt-2">
-        <b-col v-if="event.event.countdown" sm="12">
-          <Countdown :event="event.event" />
-        </b-col>
+          <b-col cols="8">
+            <div v-for="(p, i) in orderedPlacements" :key="i">
+              <h5>{{ p.description }}: {{ p.place }} / {{ p.of }}</h5>
+              <b-progress height="2rem" class="my-2">
+                <b-progress-bar
+                  :value="100 - (100 * p.place) / p.of"
+                  :max="100"
+                  :animated="p.place <= 3"
+                  :style="{ fontSize: '1.25rem' }"
+                  :variant="progressClass(1 - p.place / p.of, 2)"
+                  >{{ (1 - p.place / p.of) | percent(1) }}
+                </b-progress-bar>
+              </b-progress>
+            </div>
+          </b-col>
+        </b-row>
+      </b-card-body>
+    </b-card>
 
-        <b-col v-if="event.activity" sm="4" class="mb-2">
-          <ActivityResult :activity="event.activity" />
-        </b-col>
+    <b-card v-if="fundraising" no-body class="mb-3">
+      <b-card-header
+        ><b-button
+          class="float-right"
+          size="sm"
+          variant="outline-primary"
+          v-b-modal.addDonation
+          ><b-icon icon="plus"
+        /></b-button>
+        <h3>Fundraising</h3></b-card-header
+      >
+      <b-card-body>
+        <h4>
+          <div class="float-right">
+            {{ (fundraising.received / fundraising.minimum) | percent(1) }}
+          </div>
+          Raised {{ fundraising.received | currency | stripDecimals }} of
+          {{ fundraising.minimum | currency | stripDecimals }}
+        </h4>
 
-        <b-col v-if="event.results" sm="4" class="mb-2">
-          <EventResult
-            :results="event.results.groups"
-            :results_url="event.results.url"
-          />
-        </b-col>
-
-        <b-col
-          v-if="event.activity && event.activity.records"
-          sm="6"
-          class="mb-2"
+        <b-progress
+          v-if="fundraising.received >= fundraising.minimum"
+          height="8px"
+          class="bg-success upper-progress-bar"
         >
-          <ActivityRecords :records="event.activity.records" />
-        </b-col>
-
-        <b-col
-          v-if="event.activity && event.activity.achievements"
-          sm="3"
-          class="mb-2"
+          <b-progress-bar
+            :value="fundraising.minimum"
+            :max="fundraising.received"
+            variant="secondary"
+          >
+          </b-progress-bar>
+        </b-progress>
+        <b-progress
+          v-if="fundraising.donations"
+          height="3rem"
+          class="middle-progress-bar"
         >
-          <ActivityAchievements :records="event.activity.achievements" />
-        </b-col>
+          <b-progress-bar
+            v-for="(d, i) in donors"
+            :key="d.id"
+            :value="donorTotal(d)"
+            :max="fundraising.minimum"
+            :class="donorProgressClass(d, i)"
+            :variant="progressClass(fundraising.received / fundraising.minimum)"
+            v-b-tooltip.hover
+            :title="[d.firstname, d.lastname].join(' ')"
+            @click.native="showDonorHistory(d)"
+            >{{ sumStr(d.donations.map((v) => v.amount)) }}
+          </b-progress-bar>
+        </b-progress>
+        <b-progress v-else height="2rem" class="middle-progress-bar">
+          <b-progress-bar
+            :variant="progressClass(fundraising.received / fundraising.minimum)"
+            :value="fundraising.received"
+            :max="fundraising.minimum"
+          >
+          </b-progress-bar>
+        </b-progress>
+        <b-progress
+          v-if="fundraising.received >= fundraising.minimum"
+          height="8px"
+          class="bg-success lower-progress-bar"
+        >
+          <b-progress-bar
+            :value="fundraising.minimum"
+            :max="fundraising.received"
+            variant="secondary"
+          >
+          </b-progress-bar>
+        </b-progress>
+      </b-card-body>
+    </b-card>
 
-        <b-col v-if="event.registration.fundraising" sm="6" class="mb-2">
-          <Fundraising :fundraising="event.registration.fundraising" />
-        </b-col>
+    <AddDonation
+      :userEventActivityID="userEventActivity.id"
+      @update="updateFundraising"
+    />
 
-        <b-col v-if="event.activity && event.activity.note" sm="4" class="mb-2">
-          <Notes :text="event.activity.note" />
-        </b-col>
-      </b-row>
-    </b-container>
-    <b-container v-else>
-      <h3>{{ error }}</h3>
-    </b-container>
-  </layout-default>
+    <b-modal id="donationHistory" :title="donationHistoryTitle" size="lg">
+      <b-table
+        v-if="person"
+        :items="person.donations"
+        :fields="donorFields"
+        foot-clone
+      >
+        <template #cell(amount)="data">
+          {{ data.value | currency }}
+        </template>
+        <template #cell(date)="data">
+          {{ data.value | luxon({ input: { zone: "local" }, output: "DD" }) }}
+        </template>
+        <template #cell(event)="data">
+          {{ data.value.eventYear }} {{ data.value.eventName }}
+          <span v-if="data.value.eventActivityName"
+            >({{ data.value.eventActivityName }})</span
+          >
+        </template>
+
+        <template #foot(amount)>
+          {{
+            person.donations.reduce(
+              (a, c) => a + Number.parseFloat(c.amount),
+              0
+            ) | currency
+          }}
+        </template>
+        <template #foot(date)>Total</template>
+        <template #foot()><span></span></template>
+      </b-table>
+    </b-modal>
+  </b-container>
 </template>
 
 <script>
@@ -96,54 +253,83 @@ export default {
       title: this.title,
     };
   },
-  mixins: [Branding],
+  mixins: [Branding, EventFilters, UnitConversion],
   components: {
-    LayoutDefault,
-    SideBar,
-
-    EventDetails,
-    RegistrationStatus,
     Countdown,
-    ActivityResult,
-    ActivityRecords,
-    ActivityAchievements,
-    EventResult,
-    Fundraising,
-    Notes,
-    Links,
-    EventSequence,
+    AddDonation,
   },
   data: function () {
     return {
       event: null,
-      links: null,
-      error: "",
-      sequence: [],
-      eventSidebarOps: [
-        { text: "Edit Registration", to: { name: "edit_event" } },
-        {
-          text: "Remove from My Events",
-          to: { name: "delete_event" },
-          variant: "danger",
-        },
-      ],
+      eventActivity: null,
+      eventGroup: null,
+      eventSeries: null,
+      userEventActivity: null,
+      fundraising: null,
+      activity: null,
+
+      person: null,
+      donorFields: [{ key: "date" }, { key: "event" }, { key: "amount" }],
     };
   },
   methods: {
-    init() {
+    init: function () {
       let self = this;
       this.sequence = [];
       this.$http
-        .get("event/" + self.effectiveUser + "/" + this.$route.params.id)
-        .then(response => {
-          self.event = response.data.event;
-          self.sequence = response.data.sequence;
-          self.links = response.data.links;
+        .get(["user", "events", this.$route.params.id].join("/"))
+        .then((response) => {
+          self.userEventActivity = response.data;
+          self.eventActivity = self.userEventActivity.eventActivity;
+          delete self.userEventActivity.eventActivity;
+          self.event = self.eventActivity.event;
+          delete self.eventActivity.event;
+          self.eventSeries = self.event.eventSeries;
+          delete self.event.eventSeries;
+          self.eventGroup = self.event.eventGroup;
+          delete self.event.eventGroup;
+          self.activity = self.userEventActivity.activity;
+          delete self.userEventActivity.activity;
+          self.fundraising = self.userEventActivity.fundraising;
+          delete self.userEventActivity.fundraising;
         })
-        .catch(err => (self.error = err.response.data.message));
-    }
+        .catch((err) => (self.error = err.response.data.message));
+    },
+    updateFundraising: function (newValue) {
+      this.fundraising = newValue;
+    },
+    progressClass: function (v, ts = 1) {
+      let t = ts == 1 ? [1, 0.5] : [2 / 3, 1 / 3];
+      if (v >= t[0]) return "success";
+      if (v >= t[1]) return "warning";
+      return "danger";
+    },
+    donorTotal: function (d) {
+      let t = d.donations
+        .map((v) => v.amount)
+        .reduce((a, c) => Number.parseFloat(c) + a, 0);
+      return t;
+    },
+    sumStr: function (a) {
+      return a
+        .map((v) =>
+          this.$options.filters.stripDecimals(this.$options.filters.currency(v))
+        )
+        .join(" + ");
+    },
+    showDonorHistory: function (d) {
+      this.$http.get(["donors", d.id].join("/")).then((resp) => {
+        this.person = { ...resp.data.person, donations: resp.data.donations };
+        this.$bvModal.show("donationHistory");
+      });
+    },
+    donorProgressClass: function (d, i) {
+      let c = ["clickable-progress"];
+      if (i > 0) c.push("separated-progress");
+      return c;
+    },
   },
-  created() {
+  mounted() {
     this.init();
   },
   watch: {
@@ -155,28 +341,77 @@ export default {
     },
   },
   computed: {
+    ...mapGetters("meta", { uom: "getUnitOfMeasure" }),
     title: function () {
-      return `${this.applicationName} / Events: ${this.event.eventActivity.event.name}`;
+      if (this.event)
+        return `${this.applicationName} / Event / ${this.event.year} ${this.event.name}`;
+      else return this.applicationName;
     },
     effectiveUser: function () {
       if (this.$route.params.user) return this.$route.params.user;
       return this.$store.getters["auth/currentUser"].username;
     },
     eventIsInFuture: function () {
-      return moment(this.event.event.scheduled_start).diff(moment()) > 0;
+      return (
+        DateTime.fromISO(this.eventActivity.scheduledStart) > DateTime.local()
+      );
     },
-    sidebarOps: function () {
+    countdownTarget: function () {
+      return DateTime.fromISO(this.eventActivity.scheduledStart)
+        .setZone("local")
+        .toFormat("f");
+    },
+    orderedPlacements: function () {
       if (
-        this.event &&
-        this.event.registration.user.id ===
-          this.$store.getters["auth/currentUser"].id
+        this.userEventActivity == null ||
+        this.userEventActivity.placements == null ||
+        this.userEventActivity.placements.length == 0
       )
-        return this.eventSidebarOps;
-      return [];
+        return [];
+      return [...this.userEventActivity.placements].sort((a, b) => b.of - a.of);
+    },
+    donors: function () {
+      if (this.fundraising.donations == null) return null;
+      let donors = [];
+      this.fundraising.donations.forEach((d) => {
+        let p = donors.find((x) => x.id == d.person.id);
+        if (p == null) {
+          p = { ...d.person, adddress: d.address, donations: [] };
+          donors.push(p);
+        }
+        p.donations.push({ amount: d.amount, date: d.date });
+      });
+      return donors.sort((a, b) => this.donorTotal(a) - this.donorTotal(b));
+    },
+    donationHistoryTitle: function () {
+      let base = "Donation History";
+      if (this.person == null) return base;
+      return [
+        [this.person.firstname, this.person.lastname].join(" "),
+        base,
+      ].join("'s ");
     },
   },
 };
 </script>
 
-<style scoped>
+<style>
+.event-details legend {
+  font-weight: bold;
+}
+.clickable-progress {
+  cursor: pointer;
+}
+.separated-progress {
+  border-left: 2px solid gray;
+}
+.upper-progress-bar {
+  border-radius: 0.25rem 0.25rem 0px 0px !important;
+}
+.middle-progress-bar {
+  border-radius: 0px !important;
+}
+.lower-progress-bar {
+  border-radius: 0px 0px 0.25rem 0.25rem !important;
+}
 </style>
