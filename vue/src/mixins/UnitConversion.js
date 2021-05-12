4b344858457259f21d4b6fd305719fa8261a6e39
Mark Tyrrell <mark@digicowsoftware.com>
2021-05-12 10:02:08 -0400

    add mixin methods for comparing speeds

--- vue/src/mixins/UnitConversion.js
+++ vue/src/mixins/UnitConversion.js
@@ -17,5 +17,18 @@ export default {
         },
       };
     },
+    paceToSpeed: function (p) {
+      let multipliers = [0, 1, 60, 60, 24];
+      let bits = p.split(':').reverse();
+      let m = 1;
+      return (multipliers[2] * multipliers[3]) / bits.reduce((a, c) => { return a + multipliers[m++] * c }, 0)
+    },
+    activitySpeed: function (a) {
+      if (a) {
+        if (a.speed) return a.speed.value;
+        if (a.pace) return this.paceToSpeed(a.pace.value);
+      }
+      return null;
+    }
   }
 };
