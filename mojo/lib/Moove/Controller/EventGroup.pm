722663543b4b7b44fe0e3f19b34b5dc8edd4f95b
Mark Tyrrell <mark@digicowsoftware.com>
2021-05-14 12:49:33 -0400

    event group API endpoint

---
+++ mojo/lib/Moove/Controller/EventGroup.pm
@@ -0,0 +1,42 @@
+package Moove::Controller::EventGroup;
+use Mojo::Base 'DCS::Base::API::Model::Controller';
+use Role::Tiny::With;
+
+with 'DCS::Base::Role::Rest::Get';
+with 'Moove::Controller::Role::ModelEncoding::UserEventActivity',
+  'Moove::Controller::Role::ModelEncoding::Activity',
+  'Moove::Controller::Role::ModelEncoding::ActivityType',
+  'Moove::Controller::Role::ModelEncoding::EventPlacement',
+  'Moove::Controller::Role::ModelEncoding::EventType',
+  'Moove::Controller::Role::ModelEncoding::Registration::EventActivity';
+
+use experimental qw(signatures postderef);
+
+sub encode_model_event ($self, $event) {
+  return {
+    id      => $event->id,
+    name    => $event->name,
+    year    => $event->year,
+    address => $self->encode_model($event->address),
+  };
+}
+
+sub encode_model_eventgroup ($self, $entity) {
+  my $user = $self->current_user;
+  if (my $username = $self->validation->param('username')) {
+    $user = $self->model('User')->find({username => $username});
+  }
+
+  my @events = $entity->events_2s->search_related('event_activities')->search_related('event_registrations')
+    ->search_related('user_event_activities')->for_user($user)->visible_to($self->current_user)->all;
+
+  return {
+    id     => $entity->id,
+    name   => $entity->name,
+    year   => $entity->year,
+    url    => $entity->url,
+    events => $self->encode_model([@events])
+  };
+}
+
+1;
