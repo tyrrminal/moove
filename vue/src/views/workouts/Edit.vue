a2918e271de576883fc72b9911165bd9a0b23dc3
Mark Tyrrell <mark@digicowsoftware.com>
2021-08-12 16:22:17 -0400

    provide visual feedback on imported activities

--- vue/src/views/workouts/Edit.vue
+++ vue/src/views/workouts/Edit.vue
@@ -1,32 +1,62 @@
 <template>
-  <b-container class="mt-3">
-    <b-form @submit.prevent="uploadFile">
-      <b-row>
-        <b-col cols="2">
-          <b-select
-            v-model="type"
-            :options="dataSources"
-            value-field="id"
-            text-field="name"
-          >
-          </b-select>
-        </b-col>
-        <b-col cols="5">
-          <b-file
-            accept="application/zip"
-            v-model="upload"
-            placeholder="Choose a file or drop it here..."
-            drop-placeholder="Drop file here..."
-          ></b-file>
-        </b-col>
-        <b-col cols="1">
-          <b-button type="submit" :disabled="type == null || upload == null"
-            >Submit</b-button
-          >
+  <div>
+    <b-container class="mt-3">
+      <b-form @submit.prevent="uploadFile">
+        <b-row>
+          <b-col cols="2">
+            <b-select
+              v-model="type"
+              :options="dataSources"
+              value-field="id"
+              text-field="name"
+            >
+            </b-select>
+          </b-col>
+          <b-col cols="5">
+            <b-file
+              accept="application/zip"
+              v-model="upload"
+              placeholder="Choose a file or drop it here..."
+              drop-placeholder="Drop file here..."
+            ></b-file>
+          </b-col>
+          <b-col cols="1">
+            <b-button type="submit" :disabled="type == null || upload == null"
+              >Submit</b-button
+            >
+          </b-col>
+        </b-row>
+      </b-form>
+    </b-container>
+
+    <b-container v-if="uploaded.length > 0" class="mt-3">
+      <b-row
+        v-for="activity in uploaded.filter((a) => a.isNew)"
+        :key="activity.id"
+      >
+        <b-col
+          ><b-link :to="{ name: 'activity', params: { id: activity.id } }">{{
+            activityType(activity.activityTypeID).description
+          }}</b-link>
+          on
+          {{ activity.startTime | luxon
+          }}<b-badge class="ml-2" variant="success">NEW!</b-badge>
         </b-col>
       </b-row>
-    </b-form>
-  </b-container>
+      <b-row
+        v-for="activity in uploaded.filter((a) => !a.isNew)"
+        :key="activity.id"
+      >
+        <b-col class="text-muted"
+          ><b-link :to="{ name: 'activity', params: { id: activity.id } }">{{
+            activityType(activity.activityTypeID).description
+          }}</b-link>
+          on
+          {{ activity.startTime | luxon }}
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -38,12 +68,15 @@ export default {
     return {
       type: null,
       upload: null,
+
+      uploaded: [],
     };
   },
   computed: {
     ...mapGetters("meta", {
       getExternalDataSources: "getExternalDataSources",
       isLoaded: "isLoaded",
+      activityType: "getActivityType",
     }),
     dataSources: function () {
       return this.getExternalDataSources.filter((ds) => ds.type == "Activity");
@@ -54,9 +87,15 @@ export default {
       const formData = new FormData();
       formData.append("file", this.upload, this.upload.name);
       formData.append("externalDataSourceID", this.type);
-      this.$http.post("/activities/import", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      });
+      this.$http
+        .post("/activities/import", formData, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((resp) => {
+          let uploaded = resp.data;
+          uploaded.sort((a, b) => a.startTime.localeCompare(b.startTime));
+          this.uploaded.push(...uploaded);
+        });
     },
   },
 };
