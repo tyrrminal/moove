32c0722fcb0b635e8cd0fbfea2e8d9380e138c49
Mark Tyrrell <mark@digicowsoftware.com>
2021-08-17 16:16:15 -0400

    remove unused toolbar

--- vue/src/views/activities/Base.vue
+++ vue/src/views/activities/Base.vue
@@ -1,45 +1,9 @@
 <template>
-  <div class="mt-1">
-    <b-button-group>
-      <b-dropdown text="Cardio"
-        ><b-dropdown-item v-for="i in cardioTypes" :key="i.id">{{
-          i.description
-        }}</b-dropdown-item></b-dropdown
-      >
-      <b-dropdown text="Lifts"
-        ><b-dropdown-item v-for="i in liftTypes" :key="i.id">{{
-          i.description
-        }}</b-dropdown-item></b-dropdown
-      >
-      <b-dropdown text="Holds"
-        ><b-dropdown-item v-for="i in holdTypes" :key="i.id">{{
-          i.description
-        }}</b-dropdown-item></b-dropdown
-      >
-    </b-button-group>
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      iconScale: 1.5,
-    };
-  },
-  computed: {
-    cardioTypes: function () {
-      return [];
-    },
-    liftTypes: function () {
-      return [];
-    },
-    holdTypes: function () {
-      return [];
-    },
-  },
-};
+export default {};
 </script>
 
 <style>
