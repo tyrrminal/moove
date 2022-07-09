8c307dea44bf28fc16c084034cf766fe596c7fdd
Mark Tyrrell <mark@digicowsoftware.com>
2022-07-09 17:45:55 -0400

    add accessor to get partitions valid for input data

--- mojo/lib/Moove/Model/ResultSet/EventPlacementPartition.pm
+++ mojo/lib/Moove/Model/ResultSet/EventPlacementPartition.pm
@@ -3,6 +3,21 @@ use v5.36;
 
 use base qw(DBIx::Class::ResultSet);
 
+sub get_partitions ($self, $event_activity, $gender, $division) {
+  my %p;
+  $p{overall} = $self->app->model('EventPlacementPartition')
+    ->find_or_create({event_activity => $event_activity, gender_id => undef, division_id => undef});
+  $p{gender} =
+    $self->app->model('EventPlacementPartition')
+    ->find_or_create({event_activity => $event_activity, gender_id => $gender->id, division_id => undef})
+    if ($gender);
+  $p{division} =
+    $self->app->model('EventPlacementPartition')
+    ->find_or_create({event_activity => $event_activity, gender_id => undef, division_id => $division->id})
+    if ($division);
+  return %p;
+}
+
 sub for_event ($self, $event) {
   return $self->search(
     {
