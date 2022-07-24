5493f1a801ae41c3de75394f2770ce9cb6663cd6
Mark Tyrrell <mark@digicowsoftware.com>
2022-07-24 16:31:08 -0400

    stub in workout and activity editors

---
+++ vue/src/views/activities/Edit.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-container>New/Edit Activity</b-container>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {}
+  },
+  props: {
+    activity: {
+      type: Object,
+      default: () => { }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue/src/views/workouts/Edit.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-container>New/Edit Workout</b-container>
+</template>
+
+<script>
+export default ({
+  data: function () {
+    return {
+      edit: null
+    }
+  },
+  props: {
+    workout: {
+      type: Object,
+      default: () => { }
+    }
+  },
+  mounted: function () {
+    this.edit = cloneDeep(this.workout);
+  }
+})
+</script>
