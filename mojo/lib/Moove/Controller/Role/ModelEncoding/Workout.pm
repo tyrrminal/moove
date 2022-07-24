e91f5be0b9b9fe74517db2b6f19904a57d690b22
Mark Tyrrell <mark@digicowsoftware.com>
2022-07-24 16:15:39 -0400

    add list of activities to workout response

--- mojo/lib/Moove/Controller/Role/ModelEncoding/Workout.pm
+++ mojo/lib/Moove/Controller/Role/ModelEncoding/Workout.pm
@@ -2,13 +2,15 @@ package Moove::Controller::Role::ModelEncoding::Workout;
 use v5.36;
 
 use Role::Tiny;
+with 'Moove::Controller::Role::ModelEncoding::Activity';
 
 sub encode_model_workout ($self, $entity) {
   return {
-    id   => $entity->id,
-    date => $self->encode_date($entity->date),
-    name => $entity->name,
-    user => $self->encode_model($entity->user),
+    id         => $entity->id,
+    date       => $self->encode_date($entity->date),
+    name       => $entity->name,
+    user       => $self->encode_model($entity->user),
+    activities => $self->encode_model([$entity->activities->all]),
   };
 }
 
