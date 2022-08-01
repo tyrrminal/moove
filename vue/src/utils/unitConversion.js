b42e16fd2e61026216a46f5a3cb5670325a9b26b
Mark Tyrrell <mark@digicowsoftware.com>
2022-08-01 11:11:04 -0400

    add additional conversion functions

--- vue/src/utils/unitConversion.js
+++ vue/src/utils/unitConversion.js
@@ -1,3 +1,28 @@
+export function convertUnitValue(uv, sUnit, tUnit = null) {
+  if (tUnit != null && tUnit.id == uv.unitOfMeasureID) return uv;
+  let n = uv;
+  if (sUnit.normalUnitID != null) {
+    if (sUnit.inverted) n = 1 / n;
+    n *= sUnit.normalizationFactor;
+  }
+  if (tUnit == null || tUnit.normalUnitID == null) return n;
+  if (tUnit.inverted)
+    return tUnit.normalizationFactor / n;
+  return n / tUnit.normalizationFactor;
+};
+
+export function hmsToHours(t) {
+  let [h, m, s] = t.split(":").map(i => Number(i));
+  return h + (m / 60) + (s / (60 * 60));
+};
+
+export function minutesToHms(min) {
+  let h = Math.floor(min / 60);
+  let m = Math.floor((min - h * 60));
+  let s = Math.round((min - h * 60 - m) * 60);
+  return [h, m, s].map(x => String(x).padStart(2, '0')).join(':');
+};
+
 export function paceToSpeed(p) {
   let multipliers = [0, 1, 60, 60, 24];
   let bits = p.split(':').reverse();
