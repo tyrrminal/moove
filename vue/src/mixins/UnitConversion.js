9e1484fd3f9a44970f0ea572620c4eb9b639d4cd
Mark Tyrrell <mark@digicowsoftware.com>
2022-08-01 17:06:22 -0400

    simplify fillUnits to ONLY populate object with a full unit object

--- vue/src/mixins/UnitConversion.js
+++ vue/src/mixins/UnitConversion.js
@@ -6,16 +6,10 @@ export default {
   },
   methods: {
     fillUnits: function (d) {
-      let bu = this.getUnitOfMeasure(d.unitOfMeasureID);
-      if (bu == null) return null;
-      let nu = this.getUnitOfMeasure(bu.normalUnitID) || bu;
-      return {
-        quantity: { value: d.value, units: bu },
-        normalizedQuantity: {
-          value: d.value * bu.normalizationFactor,
-          units: nu,
-        },
-      };
+      let bu;
+      if (bu = this.getUnitOfMeasure(d.unitOfMeasureID))
+        return { value: d.value, units: bu };
+      return null;
     },
   }
 };
