e015ad99944e89aa2f81040506eb940010e5c606
Mark Tyrrell <mark@digicowsoftware.com>
2022-08-15 17:48:45 -0400

    add async minion task for processing imported job's GPS coordinates

--- mojo/lib/Moove/Task/ActivityPoint.pm
+++ mojo/lib/Moove/Task/ActivityPoint.pm
@@ -2,11 +2,38 @@ package Moove::Task::ActivityPoint;
 use v5.36;
 
 use Mojo::Base 'Mojolicious::Plugin';
+use Module::Util qw(module_path);
 
 use DCS::Constants qw(:symbols);
 
 sub register ($self, $app, $args) {
-  $app->minion->add_task(import_activity_points => sub() { });
+  $app->minion->add_task(
+    import_activity_points => sub ($job, $import_class, $path, $activity_id, $activity_result_id) {
+      my $activity_result = $app->model('ActivityResult')->find($activity_result_id);
+      die("Specified activity result id does not exist\n") unless (defined($activity_result));
+
+      require(module_path($import_class)) or die("No such import class '$import_class'\n");
+      die("'$path' does not exist or is not readable\n") unless (-f $path && -r _);
+      my $importer = $import_class->new(file => $path);
+
+      my $points = $importer->get_activity_location_points($activity_id);
+      foreach my $ap ($points->@*) {
+        my $location = $app->model('Location')->create(
+          {
+            latitude  => $ap->{lat},
+            longitude => $ap->{lon}
+          }
+        );
+        $activity_result->add_to_activity_points(
+          {
+            activity_result_id => $activity_result->id,
+            timestamp          => $ap->{time},
+            location_id        => $location->id
+          }
+        );
+      }
+    }
+  );
 
   $app->minion->add_task(
     copy_activity_points => sub ($job, $destination_activity_id, $sources) {
@@ -17,9 +44,9 @@ sub register ($self, $app, $args) {
         foreach ($source->activity_result->activity_points->all) {
           $dest->activity_result->add_to_activity_points(
             {
-              location_id        => $_->location_id,
-              timestamp          => $_->timestamp,
               activity_result_id => $dest->activity_result->id,
+              timestamp          => $_->timestamp,
+              location_id        => $_->location_id,
             }
           );
         }
