bf8a066d83f13e5dbad3910e756153d686218603
Mark Tyrrell <mark@digicowsoftware.com>
2022-08-16 12:44:53 -0400

    handle event types better

--- vue/src/views/events/Add.vue
+++ vue/src/views/events/Add.vue
@@ -3,11 +3,9 @@
     <b-row>
       <b-col cols="6">
         <b-card class="mt-2" no-body v-if="eventGroup">
-          <b-card-header
-            ><strong
-              >Parent Event
-              <b-icon v-if="eventGroup" icon="lock-fill" /></strong
-          ></b-card-header>
+          <b-card-header><strong>Parent Event
+              <b-icon v-if="eventGroup" icon="lock-fill" />
+            </strong></b-card-header>
           <b-card-body>
             <b-form-group>
               <b-input :disabled="true" :value="eventGroup.name" />
@@ -16,9 +14,9 @@
               <b-input-group>
                 <b-input :disabled="true" :value="eventGroup.url" />
                 <template #append>
-                  <b-button :href="eventGroup.url" target="_blank"
-                    ><b-icon icon="link"
-                  /></b-button>
+                  <b-button :href="eventGroup.url" target="_blank">
+                    <b-icon icon="link" />
+                  </b-button>
                 </template>
               </b-input-group>
             </b-form-group>
@@ -47,16 +45,11 @@
         </b-form-group>
         <b-form-group label="Address" label-cols="1">
           <b-input-group>
-            <b-input
-              :disabled="true"
-              :value="
+            <b-input :disabled="true" :value="
                 $options.filters.formatAddress(newEvent.event.address || {})
-              "
-            />
+            " />
             <template #append>
-              <b-button variant="warning" @click="makeNewAddress"
-                >Change</b-button
-              >
+              <b-button variant="warning" @click="makeNewAddress">Change</b-button>
             </template>
           </b-input-group>
         </b-form-group>
@@ -64,32 +57,19 @@
           <b-col cols="4">
             <b-form-group label="Results" label-cols="3" content-cols="9">
               <b-input-group>
-                <b-select
-                  :options="eventDataSources"
-                  text-field="name"
-                  value-field="id"
-                  v-model="newEvent.event.externalDataSourceID"
-                />
+                <b-select :options="eventDataSources" text-field="name" value-field="id"
+                  v-model="newEvent.event.externalDataSourceID" />
                 <template #append>
-                  <b-button
-                    variant="outline-danger"
-                    @click="newEvent.event.externalDataSourceID = null"
-                    ><b-icon icon="x"
-                  /></b-button>
+                  <b-button variant="outline-danger" @click="newEvent.event.externalDataSourceID = null">
+                    <b-icon icon="x" />
+                  </b-button>
                 </template>
               </b-input-group>
             </b-form-group>
           </b-col>
           <b-col cols="4">
-            <b-form-group
-              label="Results Event ID"
-              label-cols="4"
-              content-cols="6"
-            >
-              <b-input
-                v-model="newEvent.event.externalIdentifier"
-                :disabled="!newEvent.event.externalDataSourceID"
-              />
+            <b-form-group label="Results Event ID" label-cols="4" content-cols="6">
+              <b-input v-model="newEvent.event.externalIdentifier" :disabled="!newEvent.event.externalDataSourceID" />
             </b-form-group>
           </b-col>
         </b-row>
@@ -99,37 +79,22 @@
     <b-card class="mt-2 mb-4" no-body>
       <b-card-header><strong>Event Activities</strong></b-card-header>
       <b-card-body>
-        <div
-          v-for="(a, i) in newEvent.eventActivities"
-          :key="i"
-          class="border-left border-success mb-2 pl-2 pb-1 bg-light"
-        >
+        <div v-for="(a, i) in newEvent.eventActivities" :key="i"
+          class="border-left border-success mb-2 pl-2 pb-1 bg-light">
           <div class="float-right">
-            <b-button
-              v-if="i > 0"
-              variant="danger"
-              @click="newEvent.eventActivities.splice(i, 1)"
-              size="sm"
-              ><b-icon icon="x"
-            /></b-button>
+            <b-button v-if="i > 0" variant="danger" @click="newEvent.eventActivities.splice(i, 1)" size="sm">
+              <b-icon icon="x" />
+            </b-button>
           </div>
           <b-row class="pt-2">
             <b-col cols="4">
               <b-form-group label="Name" label-cols="3">
-                <b-input
-                  v-model="a.name"
-                  title="Required when Event has multiple activities"
-                />
+                <b-input v-model="a.name" title="Required when Event has multiple activities" />
               </b-form-group>
             </b-col>
             <b-col cols="4">
               <b-form-group label="Type" label-cols="3">
-                <b-select
-                  :options="eventTypes"
-                  text-field="description"
-                  value-field="id"
-                  v-model="a.eventType.id"
-                />
+                <b-select :options="eventTypes" text-field="description" value-field="id" v-model="a.eventType.id" />
               </b-form-group>
             </b-col>
             <b-col cols="3">
@@ -137,12 +102,8 @@
                 <b-input-group>
                   <b-input v-model="a.distance.value" />
                   <template #append>
-                    <b-select
-                      :options="unitsOfMeasure"
-                      text-field="abbreviation"
-                      value-field="id"
-                      v-model="a.distance.unitOfMeasureID"
-                    />
+                    <b-select :options="unitsOfMeasure" text-field="abbreviation" value-field="id"
+                      v-model="a.distance.unitOfMeasureID" />
                   </template>
                 </b-input-group>
               </b-form-group>
@@ -160,47 +121,23 @@
               </b-form-group>
             </b-col>
           </b-row>
-          <b-form-group
-            label="Results Activity ID"
-            label-cols="2"
-            content-cols="2"
-          >
-            <b-input
-              v-model="a.externalIdentifier"
-              :disabled="!newEvent.event.externalDataSourceID"
-            />
+          <b-form-group label="Results Activity ID" label-cols="2" content-cols="2">
+            <b-input v-model="a.externalIdentifier" :disabled="!newEvent.event.externalDataSourceID" />
           </b-form-group>
         </div>
-        <b-button
-          variant="outline-success"
-          @click="
-            newEvent.eventActivities.push({
-              eventType: 0,
-              name: null,
-              distance: { value: '', unitOfMeasureID: 1 },
-              date: '',
-              time: '',
-            })
-          "
-          ><b-icon scale="3" icon="plus" class="mr-1"></b-icon> Add an
-          Activity</b-button
-        >
+        <b-button variant="outline-success" @click="addEventActivity">
+          <b-icon scale="3" icon="plus" class="mr-1"></b-icon> Add an
+          Activity
+        </b-button>
       </b-card-body>
     </b-card>
 
-    <b-button @click="saveEvent" class="mb-4 float-right" variant="primary"
-      >Save</b-button
-    >
+    <b-button @click="saveEvent" class="mb-4 float-right" variant="primary">Save</b-button>
 
-    <b-modal
-      id="changeAddress"
-      title="Edit Address"
-      @cancel="newAddress = null"
-      @ok="
+    <b-modal id="changeAddress" title="Edit Address" @cancel="newAddress = null" @ok="
         newEvent.event.address = newAddress;
         newAddress = null;
-      "
-    >
+    ">
       <template v-if="newAddress">
         <b-form-group label="Street (1)" label-cols="2">
           <b-input v-model="newAddress.street1" />
@@ -279,6 +216,15 @@ export default {
       this.newAddress = Object.assign({}, this.newEvent.event.address);
       delete this.newAddress.id;
       this.$bvModal.show("changeAddress");
+    },
+    addEventActivity: function () {
+      this.newEvent.eventActivities.push({
+        eventType: { id: 0 },
+        name: null,
+        distance: { value: '', unitOfMeasureID: 1 },
+        date: '',
+        time: '',
+      })
     },
     saveEvent: function () {
       this.$http.post("events", this.apiRecord).then((resp) => {
