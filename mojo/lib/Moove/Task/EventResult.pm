a7cbd740da9f5e299dcf391cecc990947cec1ca6
tyrrminal <mark@tyrrminal.dev>
2022-10-23 20:28:14 -0400

    track import progress as a minion job note

--- mojo/lib/Moove/Task/EventResult.pm
+++ mojo/lib/Moove/Task/EventResult.pm
@@ -2,12 +2,14 @@ package Moove::Task::EventResult;
 use v5.36;
 
 use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::Util qw(class_to_path);
 
 use DCS::Constants qw(:symbols);
 
 sub register ($self, $app, $args) {
   $app->minion->add_task(
     import_event_results => sub ($job, $event_activity_id) {
+      $job->note(progress => 0);
       my $event_activity = $job->app->model('EventActivity')->find($event_activity_id);
       my $event          = $event_activity->event;
       my $edc            = $event->external_data_source;
@@ -15,27 +17,37 @@ sub register ($self, $app, $args) {
       require(class_to_path($class));
       my $importer = $class->new(event_id => $event->external_identifier, race_id => $event_activity->external_identifier);
 
+      # BEGIN LRP
       my @participants = $importer->results->@*;
+      $job->note(progress => $job->info->{notes}->{progress} + 25);
+      # END LRP
       return unless (@participants);
 
       my $edc_overrides = $job->app->conf->import_overrides->event_results->{$edc->name};
       my $overrides     = $edc_overrides ? $edc_overrides->{$event_activity->qualified_external_identifier} : {};
 
+      # BEGIN LRP
       $event_activity->delete_results();
       $event_activity->update({entrants => $importer->total_entrants});
       foreach my $p (@participants) {
-        process_overrides($overrides, $p);
+        apply_overrides($overrides, $p);
         $event_activity->add_participant($p);
+        $job->note(progress => $job->info->{notes}->{progress} + 70 / @participants);
       }
+      $job->note(progress => 95);
       $event_activity->update_missing_result_paces;
+      $job->note(progress => $job->info->{notes}->{progress} + 2);
       foreach my $g ($job->app->model("Gender")->all) {
         $event_activity->add_placements_for_gender($g);
+        $job->note(progress => $job->info->{notes}->{progress} + 1);
       }
+      # END LRP
+      $job->note(progress => 100);
     }
   );
 }
 
-sub process_overrides ($overrides, $record) {
+sub apply_overrides ($overrides, $record) {
   foreach my $key (keys($overrides->%*)) {
     my $replacements = $overrides->{$key};
     foreach my $v (keys($replacements->%*)) {
