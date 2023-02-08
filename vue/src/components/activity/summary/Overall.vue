0ca0a78ce8a0fb9ea86b3bd6ed3ad1e252dc9a56
tyrrminal <mark@tyrrminal.dev>
2023-02-08 09:54:47 -0500

    display loading indicators until data is available

--- vue/src/components/activity/summary/Overall.vue
+++ vue/src/components/activity/summary/Overall.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <h3>Overall</h3>
-    <span>{{ summaryData.period.daysElapsed | number("0,0") }} days ~
-      {{ summaryData.period.years | number("0,0.00") }} years</span>
-
-    <b-list-group flush>
+    <template>
+      <b-list-group flush class="mb-4">
+        <b-list-group-item>
+          <h3>All Time</h3>
+          }} days)</span>
+          <b-skeleton v-else />
+        </b-list-group-item>
+        <div v-if="loaded">
       <SummaryElement v-for="a in activities" :key="a.activityTypeID || 0" :activity="a" />
+        </div>
+        <b-list-group-item v-else class="d-flex justify-content-center mb-3">
+          <b-spinner label="Loading..."></b-spinner>
+        </b-list-group-item>
     </b-list-group>
+    </template>
+
   </div>
 </template>
 
@@ -21,10 +30,13 @@ export default {
   props: {
     summaryData: {
       type: Object,
-      required: true
+      required: false
     }
   },
   computed: {
+    loaded: function () {
+      return this.summaryData != null
+    },
     activities: function () {
       return this.summaryData.activities.filter((a) => a.distance > 0).sort((a, b) => b.distance - a.distance);
     },
