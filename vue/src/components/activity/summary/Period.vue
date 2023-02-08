59e1fd23a8c7042452d2b80862dd9125a0bca5ee
tyrrminal <mark@tyrrminal.dev>
2023-02-08 09:52:42 -0500

    received cached data from parent instead of performing load

--- vue/src/components/activity/summary/Period.vue
+++ vue/src/components/activity/summary/Period.vue
@@ -2,8 +2,8 @@
   <b-card no-body>
     <b-card-header>{{ sectionLabel }}</b-card-header>
     <b-card-body v-if="loaded">
-      <b-progress class="mb-2" :max="summaryData.period.daysTotal" :title="progressTooltip">
-        <b-progress-bar :value="summaryData.period.daysElapsed">
+      <b-progress class="mb-2" :max="periodData.period.daysTotal" :title="progressTooltip">
+        <b-progress-bar :value="periodData.period.daysElapsed">
           {{ progressText }}
         </b-progress-bar>
       </b-progress>
@@ -30,93 +30,54 @@ export default {
       type: String,
       required: true,
     },
-    startDate: {
-      type: Object,
-      required: false,
+    date: {
+      type: String,
+      required: true,
     },
-  },
-  data: function () {
-    return {
-      summaryData: {},
-      date: null,
-      loaded: false,
-    };
-  },
-  mounted: function () {
-    if (this.period)
-      this.date = (this.startDate || DateTime.local())
-        .plus({ days: this.period == "week" ? 1 : 0 })
-        .startOf(this.period)
-        .minus({ days: this.period == "week" ? 1 : 0 });
-
-    this.load();
+    summaryData: {
+      type: Object,
+      required: false
+    }
   },
   computed: {
+    loaded: function () {
+      return this.periodData != null
+    },
+    periodData: function () {
+      return this.summaryData[this.date + this.period]
+    },
     activities: function () {
-      return this.summaryData.activities.filter((a) => a.distance > 0);
+      return this.periodData.activities.filter((a) => a.distance > 0);
     },
     sectionLabel: function () {
       let l;
       switch (this.period) {
-        case "month":
-          l = DateTime.fromISO(this.date).toFormat("LLL yyyy");
-          break;
         case "week":
-          l = DateTime.fromISO(this.date).toFormat("'W'WW yyyy");
+          l = DateTime.fromISO(this.date).plus({ days: 1 }).toFormat("'W'WW kkkk");
+          break;
+        case "month":
+          l = DateTime.fromISO(this.date).toFormat("LLL");
           break;
         case "year":
           l = DateTime.fromISO(this.date).get(this.period);
           break;
-        default:
-          l = "Overall";
       }
       return l;
-    }, progressTooltip: function () {
+    },
+    progressTooltip: function () {
       return this.$options.filters.percent(
-        this.summaryData.period.daysElapsed / this.summaryData.period.daysTotal
+        this.periodData.period.daysElapsed / this.periodData.period.daysTotal
       );
     },
     progressText: function () {
       if (
-        this.summaryData.period.daysElapsed == this.summaryData.period.daysTotal
+        this.periodData.period.daysElapsed == this.periodData.period.daysTotal
       )
-        return `${this.summaryData.period.daysTotal} days`;
+        return `${this.periodData.period.daysTotal} days`;
       return [
-        this.summaryData.period.daysElapsed,
-        this.summaryData.period.daysTotal,
+        this.periodData.period.daysElapsed,
+        this.periodData.period.daysTotal,
       ].join("/");
-    },
-  },
-  methods: {
-    load: function () {
-      this.loaded = false;
-      let params = new URLSearchParams();
-      if (this.period != null) {
-        params.append("start", this.date.toISODate());
-        params.append("period", this.period);
-      }
-      this.$http
-        .get(["activities", "summary"].join("/"), { params })
-        .then((resp) => {
-          this.summaryData = resp.data;
-          this.loaded = true;
-        });
-    },
-    prevPeriod: function () {
-      let f = {};
-      f[this.period + "s"] = 1;
-      this.date = this.date.minus(f);
-    },
-    nextPeriod: function () {
-      let f = {};
-      f[this.period + "s"] = 1;
-      this.date = this.date.plus(f);
-    },
-
-  },
-  watch: {
-    date: function () {
-      this.load();
     },
   },
 };
