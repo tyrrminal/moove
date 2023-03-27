e01253faafeb694e3921fed75f698ef74db2d566
tyrrminal <mark@tyrrminal.dev>
2023-03-27 17:49:54 -0400

    create intermediate components for grouped/ungrouped lists

---
+++ vue/src/components/event/UngroupedList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Grid :events="events" date-format="DATE_MED" :separate-year="false" v-bind="gridOptions" />
+  </div>
+</template>
+
+<script>
+import Grid from "@/components/event/Grid"
+
+export default {
+  name: "UngroupedList",
+  components: {
+    Grid
+  },
+  props: {
+    events: {
+      type: Array,
+      required: true
+    },
+    loaded: {
+      type: Boolean,
+      default: true
+    },
+    gridOptions: {
+      type: Object,
+      default: () => { }
+    }
+  }
+}
+</script>
