7c8c3b3d0ec5488ed6df9a994450c1423e3c3133
tyrrminal <mark@tyrrminal.dev>
2023-07-15 23:37:46 -0400

    create backend API for event group search/create/update

--- mojo/lib/Moove/Controller/EventGroup.pm
+++ mojo/lib/Moove/Controller/EventGroup.pm
@@ -4,13 +4,19 @@ use v5.36;
 use Mojo::Base 'DCS::Base::API::Model::Controller';
 use Role::Tiny::With;
 
-with 'DCS::Base::Role::Rest::Get';
+with qw(DCS::Base::Role::Rest::List DCS::Base::Role::Rest::Create DCS::Base::Role::Rest::Entity);
 with 'Moove::Controller::Role::ModelEncoding::UserEventActivity',
   'Moove::Controller::Role::ModelEncoding::UserEventActivity::Activity',
   'Moove::Controller::Role::ModelEncoding::ActivityType',
   'Moove::Controller::Role::ModelEncoding::EventPlacement',
   'Moove::Controller::Role::ModelEncoding::EventType',
   'Moove::Controller::Role::ModelEncoding::Registration::EventActivity';
+
+sub decode_model($self, $data) {
+  my $type = delete($data->{type});
+  die("Parent-type Event Groups cannot be specific to a year") if(defined($type) && $type eq 'parent' && defined($data->{year}));
+  return $data;
+}
 
 sub encode_model_event ($self, $event) {
   return {
@@ -27,13 +33,32 @@ sub encode_model_eventgroup ($self, $entity) {
     $user = $self->model('User')->find({username => $username});
   }
 
-  return {
+  my $r = {
     id     => $entity->id,
     name   => $entity->name,
     year   => $entity->year,
     url    => $entity->url,
-    events => $self->encode_model([$entity->user_event_activities->for_user($user)->visible_to($self->current_user)->all])
   };
+
+  if(ref($self->resultset) =~ /UserEventActivity/) {
+    $r->{events} = $self->encode_model([$entity->user_event_activities->for_user($user)->visible_to($self->current_user)->all]);
+  }
+
+  return $r;
+}
+
+sub resultset ($self, @args) {
+  my $rs = $self->SUPER::resultset(@args);
+
+  if(my $name = $self->validation->param('name')) {
+    $rs = $rs->filter_name($name);
+  }
+  if(my $type = $self->validation->param('type')) {
+    $rs = $rs->type_series if($type eq 'series');
+    $rs = $rs->type_parent if($type eq 'parent');
+  }
+
+  return $rs;
 }
 
 1;
