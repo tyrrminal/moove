6f2474c82f9122e693ad065af4f2bd5be64f9c88
tyrrminal <mark@tyrrminal.dev>
2023-07-24 19:51:09 -0400

    restructure event view page with left column

--- vue/src/views/events/Detail.vue
+++ vue/src/views/events/Detail.vue
@@ -1,19 +1,18 @@
 <template>
-  <b-container>
-    <b-card v-if="event" no-body class="mt-3">
-      <b-card-header>
-        <div class="float-right">
-          <b-icon icon="compass" class="mr-1" />{{
-            event.address | formatAddress
-          }}
-        </div>
-        <b-link v-if="event.url" :href="event.url" target="_blank">{{ event.name }}</b-link><span v-else>{{ event.name
-        }}</span> - {{ event.year }}
-        <b-button :to="{ name: 'event-edit', params: { id: id } }" size="sm" variant="primary"
-          class="ml-1 text-uppercase">Edit</b-button>
-      </b-card-header>
-      <b-card-body>
+  <b-container fluid>
+    <b-row>
+      <b-col cols="3 " class="bg-light min-vh-100 pt-3">
+        <strong :style="{ fontSize: '1.2rem' }">
+          <b-link v-if="event.url" :href="event.url" target="_blank">{{ event.name }}</b-link>
+          <span v-else>{{ event.name }}</span> - {{ event.year }}
+        </strong>
 
+        <div class="my-2"><b-icon icon="compass" class="mr-1" />{{ event.address | formatAddress }}</div>
+
+        <b-button :to="{ name: 'event-edit', params: { id: id } }" size="sm" block variant="primary"
+          class="mt-2">Edit</b-button>
+      </b-col>
+      <b-col class="mt-3">
         <legend>Activities</legend>
         <b-table :items="sortedActivities" :fields="activityFields">
           <template #cell(scheduledStart)="data">
@@ -35,8 +34,9 @@
             <b-spinner v-else variant="secondary" small></b-spinner>
           </template>
         </b-table>
-      </b-card-body>
-    </b-card>
+
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
