1d1d6cfa455b9cf6ab564fd5f627033dec7676c0
tyrrminal <mark@tyrrminal.dev>
2023-08-01 14:11:45 -0400

    use the correct formatter

--- mojo/lib/Moove/Model/ResultSet/UserNominalActivity.pm
+++ mojo/lib/Moove/Model/ResultSet/UserNominalActivity.pm
@@ -3,7 +3,7 @@ use v5.36;
 
 use base qw(DBIx::Class::ResultSet);
 
-use DateTime::Format::ISO8601;
+use DateTime::Format::MySQL;
 use List::Util qw(sum);
 
 sub for_type($self, $activity_type) {
@@ -14,7 +14,7 @@ sub for_type($self, $activity_type) {
 }
 
 sub in_range($self, $start, $end) {
-  my $range = [DateTime::Format::ISO8601->format_datetime($start), DateTime::Format::ISO8601->format_datetime($end)];
+  my $range = [DateTime::Format::MySQL->format_date($start), DateTime::Format::MySQL->format_date($end)];
   $self->search({
     -or => [
       {start_date => {-between => $range}},
