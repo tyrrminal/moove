cdb3d5a912e8d7e6fc4266cd658f3d9454399876
tyrrminal <mark@tyrrminal.dev>
2023-08-09 17:35:08 -0400

    convert CityService helper to class syntax

--- mojo/lib/Moove/Import/Helper/CityService.pm
+++ mojo/lib/Moove/Import/Helper/CityService.pm
@@ -1,66 +1,48 @@
-package Moove::Import::Helper::CityService;
 use v5.38;
+use experimental qw(class);
 
-use Moose;
+class Moove::Import::Helper::CityService {
+  use Mojo::JSON qw(from_json);
+  use Readonly;
 
-use Mojo::JSON qw(from_json);
-use Readonly;
+  Readonly::Scalar my $GIT_API_BASE   => q{https://api.github.com/};
+  Readonly::Scalar my $GIT_USER_GISTS => q{users/%s/gists};
+  Readonly::Scalar my $GIT_GIST       => q{gists/%s};
 
-Readonly::Scalar my $GIT_API_BASE   => q{https://api.github.com/};
-Readonly::Scalar my $GIT_USER_GISTS => q{users/%s/gists};
-Readonly::Scalar my $GIT_GIST       => q{gists/%s};
+  Readonly::Scalar my $CITYLIST_AUTHOR   => 'tyrrminal';
+  Readonly::Scalar my $CITYLIST_FILENAME => 'usaCities.json';
 
-Readonly::Scalar my $CITYLIST_AUTHOR   => 'tyrrminal';
-Readonly::Scalar my $CITYLIST_FILENAME => 'usaCities.json';
+  Readonly::Scalar my $STATELIST_AUTHOR   => 'mshafrir';
+  Readonly::Scalar my $STATELIST_FILENAME => 'states_hash.json';
 
-Readonly::Scalar my $STATELIST_AUTHOR   => 'mshafrir';
-Readonly::Scalar my $STATELIST_FILENAME => 'states_hash.json';
+  field @city_list;
 
-has 'city_list' => (
-  is       => 'ro',
-  isa      => 'ArrayRef[HashRef[Str]]',
-  init_arg => undef,
-  builder  => '_build_city_list'
-);
+  ADJUST {
+    my %state_list = reverse from_json(get_gist_content($STATELIST_AUTHOR, $STATELIST_FILENAME))->%*;
+    @city_list = from_json(get_gist_content($CITYLIST_AUTHOR, $CITYLIST_FILENAME))->@*;
 
-sub _build_city_list {
-  my $self = shift;
-
-  my %sl = reverse %{from_json(_get_gist_content($STATELIST_AUTHOR, $STATELIST_FILENAME))};
-  my $cl = from_json(_get_gist_content($CITYLIST_AUTHOR, $CITYLIST_FILENAME));
-
-  foreach (@$cl) {
-    $_->{state_abbrv} = $sl{$_->{state}};
+    $_->{state_abbrv} = $state_list{$_->{state}} foreach (@city_list);
   }
-  return $cl;
+
+  sub get_gist_content($author, $filename) {
+    my $ua     = Mojo::UserAgent->new();
+    my $res    = $ua->get(sprintf($GIT_API_BASE . $GIT_USER_GISTS, $author))->result;
+    my ($gist) = grep {exists($_->{files}->{$filename})} $res->json->@*;
+
+    $res = $ua->get(sprintf($GIT_API_BASE . $GIT_GIST, $gist->{id}))->result;
+    return $res->json->{files}->{$filename}->{content};
+  }
+
+  method combos_with_city($city) {
+    return grep {$_->{city} eq $city} @city_list;
+  }
+
+  method is_valid_city_state($city, $state) {
+    (grep {
+      lc($_->{city}) eq lc($city) && 
+        ( lc($_->{state}) eq lc($state) || 
+         (defined($_->{state_abbrv}) && lc($_->{state_abbrv}) eq lc($state)) )
+    } @city_list) > 0;
+  }
+
 }
-
-sub _get_gist_content {
-  my ($author, $filename) = @_;
-
-  my $ua     = Mojo::UserAgent->new();
-  my $res    = $ua->get(sprintf($GIT_API_BASE . $GIT_USER_GISTS, $author))->result;
-  my ($gist) = grep {exists($_->{files}->{$filename})} @{$res->json};
-
-  $res = $ua->get(sprintf($GIT_API_BASE . $GIT_GIST, $gist->{id}))->result;
-  return $res->json->{files}->{$filename}->{content};
-}
-
-sub combos_with_city {
-  my $self = shift;
-  my ($city) = @_;
-
-  return grep {$_->{city} eq $city} @{$self->city_list};
-}
-
-sub is_valid_city_state {
-  my $self = shift;
-  my ($city, $state) = @_;
-
-  grep {
-    lc($_->{city}) eq lc($city)
-      && (lc($_->{state}) eq lc($state) || (defined($_->{state_abbrv}) && lc($_->{state_abbrv}) eq lc($state)))
-  } @{$self->city_list};
-}
-
-1;
