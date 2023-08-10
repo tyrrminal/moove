0fa6369cbbf52fdcc5614123fb442d23a5b6eeca
tyrrminal <mark@tyrrminal.dev>
2023-08-10 08:21:41 -0400

    guess we're not allowed to do this

--- mojo/lib/Moove/Import/Activity/RunKeeper.pm
+++ mojo/lib/Moove/Import/Activity/RunKeeper.pm
@@ -91,7 +91,7 @@ class Moove::Import::Activity::RunKeeper {
     return [map {@{$_->{points}}} map {@{$_->{segments}}} @{$gpx->tracks}];
   }
 
-  my method calculate_gross_time ($activity) {
+  method calculate_gross_time ($activity) {
     unzip($file => \my $data, Name => $activity->{gpx});
     my $gpx = Geo::Gpx->new(xml => $data);
 
