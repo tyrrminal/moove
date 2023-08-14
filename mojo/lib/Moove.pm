a92c44718f546e876e917c79ee38f58cd0da28dc
tyrrminal <mark@tyrrminal.dev>
2023-08-14 10:16:52 -0400

    retain v5.38 declaration in generated source files

--- mojo/lib/Moove.pm
+++ mojo/lib/Moove.pm
@@ -5,13 +5,20 @@ use Mojo::Base 'Mojolicious';
 
 use Mojolicious::Plugin::DCS::Base::Constants qw(:dbix);
 
+use DCS::Constants qw(:symbols);
+
 # This method will run once at server start
 sub startup ($self) {
   $self->plugin(
     'DCS::Base',
     cron          => undef,
     persistentlog => undef,
-    datastore     => {load_schema_options => {components => [@DEFAULT_DBIX_COMPONENTS, qw(InflateColumn::Time)]}},
+    datastore     => {load_schema_options => {
+      components => [@DEFAULT_DBIX_COMPONENTS, qw(InflateColumn::Time)],
+      filter_generated_code   => sub ($type, $class, $text) {
+        return join($NEWLINE, '#<<<', $text . '#>>>', 'use v5.38;');
+      }
+    }},
   );
   $self->plugin('Moove::Plugin::Minion');
   $self->plugin('Minion::Admin');
