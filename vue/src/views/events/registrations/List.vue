41b8b1b18c212b78c05998a95552c9f5fe68a3bb
tyrrminal <mark@tyrrminal.dev>
2023-12-11 16:54:53 -0500

    add filtering of event list by race distance

--- vue/src/views/events/registrations/List.vue
+++ vue/src/views/events/registrations/List.vue
@@ -1,11 +1,14 @@
 <template>
   <b-container fluid>
-    <b-row>
+    <b-row v-if="isMetaLoaded">
       <b-col cols="2" class="bg-sidebar min-vh-100">
         <div class="sticky-top">
           <div class="bg-white mt-2 py-2 pl-3 rounded-lg rounded-top">
             <label class="font-weight-bold" :style="{ fontSize: '10pt' }">Filters</label>
             <EventTypeSelector v-model="eventTypes" class="mb-2 d-block" />
+            <multiselect v-if="distanceOptions.length > 1 && isMetaLoaded" v-model="selectedDistances"
+              :options="distanceOptions" label="text" track-by="id" class="mb-2" :close-on-select="false"
+              :multiple="true" />
             <b-button-group size="sm" v-if="hasPrivateEvents" class="d-block">
               <b-button :pressed.sync="nonprivate" variant="outline-secondary">Public</b-button>
               <b-button :pressed.sync="private" variant="outline-danger">Private</b-button>
@@ -20,7 +23,7 @@
             <EventSummary :events="events" />
             <template #loading>
               <b-list-group class="mt-2 mb-4">
-                <b-list-group-item v-for="(s, i) in [[90, 50], [55, 66], [70, 80]]" :key="i">
+                <b-list-group-item v-for="( s, i ) in  [[90, 50], [55, 66], [70, 80]] " :key="i">
                   <b-skeleton :width="s[0] + '%'" />
                   <b-skeleton :width="s[1] + '%'" type="input" />
                 </b-list-group-item>
@@ -39,7 +42,7 @@
           v-model="eventActivityName" :style="{ width: '8rem' }" class="mb-2 ml-2" />
         <component :is="listView" :events="filteredEvents" :loaded="loaded" :gridOptions="gridOptions" />
 
-        <div v-if="eventGroupMode && loaded && filteredEvents.length > 1" class="events-chart-container">
+        <div v-if="view.splitYears == false && loaded && filteredEvents.length > 1" class="events-chart-container mb-3">
           <component :is="chartType" :data="chartData" />
         </div>
       </b-col>
@@ -48,6 +51,9 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import Multiselect from 'vue-multiselect'
+
 import Branding from "@/mixins/Branding.js";
 import Events from "@/mixins/events/API.js";
 
@@ -56,13 +62,14 @@ import EventSummary from "@/components/event/Summary.vue";
 import YearGroupedList from "@/components/event/YearGroupedList.vue";
 import UngroupedList from "@/components/event/UngroupedList.vue"
 
-import ActivityChart from "@/components/activity/charts/Annual.vue";
+import ActivityChart from "@/components/activity/charts/Speed.vue";
 import ResultsChart from "@/components/event/charts/Results.vue";
 import FundraisingChart from "@/components/event/charts/Fundraising.vue";
 
 export default {
   name: "EventRegistrationList",
   components: {
+    Multiselect,
     EventTypeSelector,
     EventSummary,
     YearGroupedList,
@@ -99,6 +106,7 @@ export default {
         distanceType: false,
         splitYears: true,
       },
+      selectedDistances: [],
     };
   },
   props: {
@@ -138,12 +146,15 @@ export default {
             self.loadEvents(pageNum + 1);
           else
             self.loaded = true
-          if (self.eventGroupMode)
+          if (self.eventGroupMode) {
+            self.$http.get("/eventgroups/" + self.params.eventGroupID).then(resp => this.listTitle = resp.data.name)
             this.listTitle = self.events[0].eventActivity.event.eventGroup.name
+          }
         });
     },
   },
   computed: {
+    ...mapGetters("meta", { getUnitOfMeasure: "getUnitOfMeasure", isMetaLoaded: "isLoaded" }),
     eventGroupMode: function () {
       return this.$route.name == 'registration-series'
     },
@@ -186,7 +197,7 @@ export default {
       }
       return ["All", ...this.events.map(e => e.eventActivity ? e.eventActivity.name : null).filter(n => n != null).filter(unique)]
     },
-    filteredEvents: function () {
+    prefilteredEvents: function () {
       let events = this.events;
       if (Object.keys(this.eventTypes).length) events = events.filter(e => this.eventTypes[e.eventActivity.eventType.id])
       if (this.view.type == 'results') events = events.filter((e) => e.placements);
@@ -195,6 +206,27 @@ export default {
       if (this.filters.private === false) events = events.filter((e) => e.visibilityTypeID > 1)
       if (this.eventActivityName != 'All') events = events.filter((e) => e.eventActivity.name == this.eventActivityName)
 
+      return events;
+    },
+    distanceOptions: function () {
+      let self = this;
+      function uniqueBy(a, cond) { return a.filter((e, i) => a.findIndex(e2 => cond(e, e2)) === i); }
+      let distances = uniqueBy(this.prefilteredEvents.map(e => {
+        let uom = self.getUnitOfMeasure(e.eventActivity.distance.unitOfMeasureID);
+        return {
+          id: e.eventActivity.distance.id,
+          value: e.eventActivity.distance.value,
+          unitOfMeasure: uom,
+          normalizedValue: e.eventActivity.distance.value * uom.normalizationFactor,
+          text: `${e.eventActivity.distance.value} ${uom.abbreviation}`,
+        }
+      }), (o1, o2) => o1.id == o2.id)
+      distances.sort((a, b) => a.normalizedValue > b.normalizedValue)
+      return distances
+    },
+    filteredEvents: function () {
+      let events = this.prefilteredEvents;
+      if (this.selectedDistances.length) events = events.filter((e) => this.selectedDistances.map(d => d.id).includes(e.eventActivity.distance.id))
       return events;
     },
     chartData: function () {
@@ -229,6 +261,9 @@ export default {
   }
 };
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css">
+</style>
 
 <style scoped>
 .bg-sidebar {
